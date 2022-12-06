ee3bbc627efd0042654aa30937a33f597ba1b8f0
User <y55mun33@gmail.com>
2022-12-06 20:48:12 +0900

    마지막 print 할 때 result 를 안해서 값이 이상하게 나왔었음

--- DFS/1_2667.py
+++ DFS/1_2667.py
@@ -1,40 +1,48 @@
 """
 1. 아이디어
-- 2중 for문, 값1 && 방문X => DFS
-- DFS 를 통해 찾은 값을 저장 후 정렬해서 출력
-
+- 2중 for, 값 1 && 방문X => DFS
+- DFS를 통해 찾은 값을 저장 후 정렬 해서 출력
 2. 시간복잡도
-- DFS: O(V+E)
-- V, E: N^2, 4N^2
-- V+E = 5N^2~=N^2 ~= 625 (가능
-
+- DFS : O(V+E)
+- V, E : N^2, 4N^2
+- V+E : 5N^2 ~= N^2 ~= 625 >> 가능
 3. 자료구조
-- 그래프 저장: int[][]
-- 방문 여부: bool[][]
-- 결과값: int[]
-
+- 그래프 저장 : int[][]
+- 방문여부 : bool[][]
+- 결과값 : int[]
 """
+
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 map = [list(map(int, input().strip())) for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
+chk = [[False] * N for _ in range(N)]
 result = []
 each = 0
-
 dy = [0,1,0,-1]
 dx = [1,0,-1,0]
 
+def dfs(y, x):
+    global each
+    each += 1
+    for k in range(4):
+        ny = y + dy[k]
+        nx = x + dx[k]
+        if 0<=ny<N and 0<=nx<N:
+            if map[ny][nx] == 1 and chk[ny][nx] == False:
+                chk[ny][nx] = True
+                dfs(ny, nx)
+
 for j in range(N):
     for i in range(N):
-        if map[j][i] == 1 and not visited[j][i]:
-            visited[j][i] = True
+        if map[j][i] == 1 and chk[j][i] == False:
+            chk[j][i] = True
             each = 0
             dfs(j,i)
             result.append(each)
 
 result.sort()
 print(len(result))
-for i in result:
-    print(i)
+for i in range(len(result)):
+    print(result[i])

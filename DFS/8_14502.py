587c9d65e02953a68cd816905ea911a7f9407038
User <y55mun33@gmail.com>
2022-12-19 21:10:42 +0900

    .

--- DFS/8_14502.py
+++ DFS/8_14502.py
@@ -6,29 +6,29 @@ for _ in range(n):
     data.append(list(map(int, input().split())))
 
 # 4가지 이동 방향에 대한 리스트
-dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
+dx = [-1, 0, 1, 0]
 
 result = 0
 
 # 깊이 우선 탐색(DFS)을 이용해 각 바이러스가 사방으로 퍼지도록 하기
-def virus(x, y):
+def virus(y,x):
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         # 상, 하, 좌, 우 중에서 바이러스가 퍼질 수 있는 경우
         if nx >= 0 and nx < n and ny >= 0 and ny < m:
-            if temp[nx][ny] == 0:
+            if temp[ny][nx] == 0:
                 # 해당 위치에 바이러스 배치하고, 다시 재귀적으로 수행
-                temp[nx][ny] = 2
-                virus(nx, ny)
+                temp[ny][nx] = 2
+                virus(ny, nx)
 
 # 현재 맵에서 안전 영역의 크기 계산하는 메서드
 def get_score():
     score = 0
-    for i in range(n):
-        for j in range(m):
-            if temp[i][j] == 0:
+    for j in range(m):
+        for i in range(n):
+            if temp[j][i] == 0:
                 score += 1
     return score
 
@@ -37,25 +37,25 @@ def dfs(count):
     global result
     # 2. 울타리가 3개 설치된 경우 바이러스 전파
     if count == 3:
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = data[i][j]
+        for j in range(m):
+            for i in range(n):
+                temp[j][i] = data[j][i]
         # 각 바이러스의 위치에서 전파 진행
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 2:
-                    virus(i, j)
+        for j in range(m):
+            for i in range(n):
+                if temp[j][i] == 2:
+                    virus(j, i)
         # 3. 안전 영역의 최대값 계산
         result = max(result, get_score())
         return
     # 1. 빈 공간에 울타리를 설치
-    for i in range(n):
-        for j in range(m):
-            if data[i][j] == 0:
-                data[i][j] = 1
+    for j in range(m):
+        for i in range(n):
+            if data[j][i] == 0:
+                data[j][i] = 1
                 count += 1
                 dfs(count)
-                data[i][j] = 0
+                data[j][i] = 0
                 count -= 1
 
 dfs(0)

cbdf8e2998f32844700a26e2612cb4d60ca408cd
User <y55mun33@gmail.com>
2022-12-20 22:07:37 +0900

    comment 1389

--- BFS/8_1389.py
+++ BFS/8_1389.py
@@ -1,3 +1,13 @@
+""" 백준 1389번 케빈 베이컨의 6단계 법칙
+
+- bfs 탐색을 통해 문제를 수행한다.
+- 반복문을 통해 모든 친구를 기준으로 탐색한다.
+- 친구 관계를 확인하고 탐색하지 않은 친구라면 탐색한다.
+- 탐색하면서 탐색하기까지 걸린 횟수를 체크한다.
+- 케벤 베이컨의 수가 가장 작은 사람을 인덱스를 이용해서 출력한다.
+
+"""
+
 from collections import deque
 
 def BFS(i):
@@ -6,7 +16,10 @@ def BFS(i):
     visited[i] = 1
     while queue:
         t = queue.popleft()
+
+        # 친구 관계를 확인하고 탐색하지 않은 친구라면 탐색한다.
         for v in edge_list[t]:
+            # 탐색하기 위한 횟수를 체크한다.
             if not visited[v]:
                 visited[v] = visited[t]+1
                 queue.append(v)
@@ -21,8 +34,11 @@ for _ in range(M):
     edge_list[e].append(s)
 
 queue = deque()
-res = []
+res = []    # 케빈 베이컨의 수를 담는 리스트
+
+# 반복문을 통해 모든 친구를 탐색한다.
 for i in range(1, N+1):
     res.append(BFS(i))
 
+# 가장 작은 케빈 베이컨의 수를 가지고 있는 사람의 인덱스 + 1 을 해주어 출력한다.
 print(res.index(min(res))+1)

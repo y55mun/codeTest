0e73abfdfe85c26ad824efe9784484f05a0101c2
User <y55mun33@gmail.com>
2022-12-30 00:06:30 +0900

    3184

--- BFS/11_3184.py
+++ BFS/11_3184.py
@@ -1,43 +1,41 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
+# BFS 이동
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
+
 
 def bfs(x, y):
-    global o, v
-    q = deque()
-    q.append([x, y])
-    to, tv = 0, 0
-    if s[x][y] == "o": to += 1
-    elif s[x][y] == "v": tv += 1
-    while q:
-        x, y = q.popleft()
+    queue = [(x, y)]
+    o, v = 0, 0
+    while queue:
+        x, y = queue.pop()
+        if board[x][y] == 'v':
+            v += 1
+        if board[x][y] == 'o':
+            o += 1
+        board[x][y] = '#'
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < r and 0 <= ny < c and visit[nx][ny] == 0 and s[nx][ny] != "#":
-                if s[nx][ny] == "o": to += 1
-                if s[nx][ny] == "v": tv += 1
-                visit[nx][ny] = 1
-                q.append([nx, ny])
-    if to and tv:
-        if to > tv: v -= tv
-        else: o -= to
 
-r, c = map(int, input().split())
-s = []
-visit = [[0] * c for i in range(r)]
-o, v = 0, 0
-for i in range(r):
-    a = list(input().strip())
-    for j in range(c):
-        if a[j] == "o": o += 1
-        if a[j] == "v": v += 1
-    s.append(a)
-for i in range(r):
-    for j in range(c):
-        if (s[i][j] == "o" or s[i][j] == "v") and visit[i][j] == 0:
-            visit[i][j] = 1
-            bfs(i, j)
-print(o, v)
+            if 0 <= nx < M and 0 <= ny < N:
+                if board[nx][ny] != '#':
+                    queue.append((nx, ny))
+    if o <= v:
+        o = 0
+    else:
+        v = 0
+    return o, v
+
+
+M, N = map(int, input().split())
+board = [list(input()) for _ in range(M)]
+O, V = 0, 0
+for i in range(M):
+    for j in range(N):
+        if board[i][j] == 'v' or board[i][j] == 'o':
+            o, v = bfs(i, j)
+            O += o
+            V += v
+
+print(O, V)

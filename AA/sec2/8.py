763941bcd9a62ab339128771e064a42725be45d3
User <y55mun33@gmail.com>
2023-01-18 22:18:48 +0900

    완료

--- AA/sec2/8.py
+++ AA/sec2/8.py
@@ -37,12 +37,20 @@ def reverse(x):
         t = x%10
         res = res * 10 + t
         x = x // 10
-
     return res
+
+def isPrime(x):
+    if x == 1:
+        return False
+
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
 
 for x in number:
     tmp = reverse(x)
 
-
-# 소수인지 판별
-#def isPrime(x):
+    if isPrime(tmp):
+        print(tmp, end=' ')

eca919461770a64a8c235513ff65324c3c6c8554
User <y55mun33@gmail.com>
2023-01-29 17:38:39 +0900

    sort 함수 사용하지 않고 구현

--- AA/sec3/4.py
+++ AA/sec3/4.py
@@ -23,12 +23,25 @@
 """
 import sys
 
-for _ in range(2):
-    n = int(input())
-    a = input().split()
-    m = int(input())
-    b = input().split()
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+p1 = p2 = 0
+c = []
 
-    c= a+b
-    c.sort()
-    print(c)
+while p1<n and p2<m:
+    if a[p1] < b[p2]:
+        c.append(a[p1])
+        p1 += 1
+    else:
+        c.append(b[p2])
+        p2 += 1
+
+if p1<n:
+    c = c+a[p1:]
+if p2<m:
+    c = c+b[p2:]
+
+for x in c:
+    print(x, end=' ')

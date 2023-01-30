ca2852b16ba98e289e9dfc4204c32234ce2160be
User <y55mun33@gmail.com>
2023-01-30 19:52:34 +0900

    해결

--- AA/sec3/5.py
+++ AA/sec3/5.py
@@ -26,18 +26,18 @@ tot = a[0]
 cnt = 0
 
 while True:
-    if tot < m:
-        if rt < n:
-            tot += a[rt]
-            rt += 1
+    if tot<m:
+        if rt<n:
+            tot+=a[rt]
+            rt+=1
         else:
             break
-    elif tot == m:
-        cnt += 1
-        tot += a[lt]
-        lt += 1
+    elif tot==m:
+        cnt+=1
+        tot-=a[lt]
+        lt+=1
     else:
-        tot-= a[lt]
-        lt += 1
+        tot-=a[lt]
+        lt+=1
 
 print(cnt)

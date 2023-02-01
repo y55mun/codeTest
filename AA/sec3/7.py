f5a4f562dbd002c221dd577cb0fb2deeb6ce4c85
User <y55mun33@gmail.com>
2023-02-01 22:12:32 +0900

    init

--- AA/sec3/7.py
+++ AA/sec3/7.py
@@ -30,3 +30,18 @@
 
 n = int(input())
 a = [list(map(int, input().split())) for _ in range(n)]
+res = 0
+
+s = e = n//2
+
+for i in range(n):  #행
+    for j in range(s, e+1): #열
+        res += a[i][j]
+    if i<n//2:  # 옆으로 길어짐
+        s -= 1
+        e += 1
+    else:   # 좁아짐
+        s += 1
+        e -= 1
+
+print(res)

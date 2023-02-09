f9e9cd3e0f13de0021e337392999d0dca67293d9
User <y55mun33@gmail.com>
2023-02-10 06:36:07 +0900

    끝

--- AA/sec3/10.py
+++ AA/sec3/10.py
@@ -1,6 +1,3 @@
-// 40점짜리
-
-
 """스토쿠 검사
 스도쿠는 매우 간단한 숫자 퍼즐이다. 9×9 크기의 보드가 있을 때, 각 행과 각 열, 그리고 9개의 3×3 크기의 보드에 1부터 9까지의 숫자가 중복 없이 나타나도록 보드를 채우면 된다.
 예를 들어 다음을 보자.
@@ -32,29 +29,30 @@ YES
 
 """
 
+def check(a):
+    for i in range(9):
+        ch1 = [0] * 10
+        ch2 = [0] * 10
+
+        for j in range(9):
+            ch1[a[i][j]] = 1
+            ch2[a[j][i]] = 1
+        if sum(ch1) != 9 or sum(ch2) != 9:
+            return False
+
+    for i in range(3):
+        for j in range(3):
+            ch3 = [0] * 10
+
+            for k in range(3):
+                for s in range(3):
+                    ch3[a[i*3+k][j*3+s]] = 1
+            if sum(ch3) != 9:
+                return False
+    return True
+
 a = [list(map(int, input().split())) for _ in range(9)]
-
-for i in range(9):
-    row_num = [0] * 10
-    col_num = [0] * 10
-
-    for j in range(9):
-        row = a[i][j]   # 가로
-        col = a[j][i]   # 세로
-
-        if row_num[row]: res = "NO"
-        if col_num[col]: res = "No"
-
-        row_num[row] = 1
-        col_num[col] = 1
-
-        if i%3 == 0 and j%3 == 0:
-            square = [0] * 10
-            for r in range(i, i+3):
-                for c in range(j, j+3):
-                    num = a[r][c]
-                    if square[num]: res = "NO"
-                    square[num] = 1
-res = "YES"
-
-print(res)
+if check(a):
+    print("YES")
+else:
+    print("NO")

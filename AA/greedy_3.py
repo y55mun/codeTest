a485fd436e03afda023108540cc2fda2fbdfd8e3
User <y55mun33@gmail.com>
2023-02-21 00:01:11 +0900

    풀이

--- AA/greedy_3.py
+++ AA/greedy_3.py
@@ -29,3 +29,17 @@
 알고리즘:
 두 수에 대하여 연산 수행 시, 두 수 중에서 하나라도 1 이하인 경우에는 더하며, 두 수가 모두 2 이상인 경우에는 곱함
 """
+
+data = input()
+
+result = int(data[0])
+
+for i in range(1, len(data)):
+
+    num = int(data[i])
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
+
+print(result)

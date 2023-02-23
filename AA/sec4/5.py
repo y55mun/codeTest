8e97c023d35363440d7b2727d650d20bf28c11ce
User <y55mun33@gmail.com>
2023-02-23 22:49:10 +0900

    주석

--- AA/sec4/5.py
+++ AA/sec4/5.py
@@ -31,10 +31,12 @@ cf) 예제설명
 
 data = input()
 
+# 첫 번째 문자를 숫자로 변경하여 대입
 result = int(data[0])
 
 for i in range(1, len(data)):
 
+    # 두 수 중에서 하나라도 0 혹은 1인 경우, 곱하기 보다는 더하기 수행
     num = int(data[i])
     if num <= 1 or result <= 1:
         result += num

e768720020fd0ba06ae167e4e07d9a54d6e6df86
User <y55mun33@gmail.com>
2023-03-07 23:23:12 +0900

    끝!

--- AA/sec5/5.py
+++ AA/sec5/5.py
@@ -30,3 +30,9 @@ n, k = map(int, input().split())
 prince = list(range(1, n+1))
 prince = deque(prince)
 
+while(prince):
+    for _ in range(k-1):
+        prince.append(prince.popleft())
+    prince.popleft()
+    if len(prince) == 1:
+        print(prince[0])

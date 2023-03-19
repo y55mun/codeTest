c6f03ecb61568384c8c69f12553cdb5c93a08c49
User <y55mun33@gmail.com>
2023-03-19 22:47:51 +0900

    끝

--- AA/sec6/3.py
+++ AA/sec6/3.py
@@ -22,6 +22,15 @@
 """
 def dfs(v):
     if v == n+1:
+        for i in range(1, n+1):
+            if ch[i] == 1:
+                print(i, end=' ')
+        print()
+    else:
+        ch[v] = 1
+        dfs(v+1)
+        ch[v] = 0
+        dfs(v+1)
 
 
 n = int(input())

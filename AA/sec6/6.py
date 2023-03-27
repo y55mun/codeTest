024eb4e67555b896d11808d3f8be666e9de2692e
User <y55mun33@gmail.com>
2023-03-27 21:12:43 +0900

    .

--- AA/sec6/6.py
+++ AA/sec6/6.py
@@ -27,6 +27,19 @@
 """
 
 
+def dfs(l):
+    global cnt
+    if l == m:
+        for j in range(m):
+            print(res[j], end=' ')
+        print()
+        cnt += 1
+
+    else:
+        for i in range(1, n + 1):
+            res[l] = i
+            dfs(l + 1)
+
 
 n, m = map(int, input().split())
 res = [0] * m

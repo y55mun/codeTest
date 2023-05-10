72389c6d91c5dd3303ef8e8d285b68181bb925e3
User <y55mun33@gmail.com>
2023-05-11 00:37:57 +0900

    init

---
+++ programmers/9.py
@@ -0,0 +1,23 @@
+'''
+현대-softeer
+근무 시간
+
+'''
+
+import sys
+
+result = 0
+
+for _ in range(5):
+    s, e = sys.stdin.readline().split()
+    stime, smin = s.split(":")
+    etime, emin = e.split(":")
+    stime, smin, etime, emin = int(stime), int(smin), int(etime), int(emin)
+
+    if emin < smin:
+        etime -= 1
+        emin += 60
+
+    result += (etime - stime) + (emin - smin) * 60
+
+print(result)

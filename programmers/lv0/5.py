68c9fa92967d04fc9d044684e0244b79f08dc661
User <y55mun33@gmail.com>
2023-06-14 21:40:03 +0900

    풀이 완료 - eval 함수 사용

--- programmers/lv0/5.py
+++ programmers/lv0/5.py
@@ -3,32 +3,5 @@ https://school.programmers.co.kr/learn/courses/30/lessons/181934
 
 '''
 
-
 def solution(ineq, eq, n, m):
-    answer = 0
-    compare = ''
-
-    if ineq == '>' and eq == '=':
-        compare = '>='
-        if n >= m:
-            return 1
-        else:
-            return 0
-    if ineq == '<' and eq == '=':
-        compare = '<='
-        if n <= m:
-            return 1
-        else:
-            return 0
-    if ineq == '>' and eq == '!':
-        compare = '>'
-        if n > m:
-            return 1
-        else:
-            return 0
-    if ineq == '<' and eq == '!':
-        compare = '<'
-        if n < m:
-            return 1
-        else:
-            return 0
+    return int(eval(str(n)+ineq+eq.replace('!', '')+str(m)))

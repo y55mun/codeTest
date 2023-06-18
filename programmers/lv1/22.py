c107a47b9e2c09073d0a7721bbc570d1a4eae0bc
User <y55mun33@gmail.com>
2023-06-18 20:57:42 +0900

    오류 - row, col 초기화 시 [] 두 번 사용해야함

--- programmers/lv1/22.py
+++ programmers/lv1/22.py
@@ -5,7 +5,7 @@ https://school.programmers.co.kr/learn/courses/30/lessons/161990
 
 
 def solution(wallpaper):
-    row, col = []
+    row, col = [], []
 
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[0])):

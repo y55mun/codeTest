a66dc017187bf6b5fca731bfc3849e7153458b1c
User <y55mun33@gmail.com>
2023-06-22 23:15:02 +0900

    풀이완료

--- AA/sec3/5.py
+++ AA/sec3/5.py
@@ -19,3 +19,25 @@ A[N]이 공백으로 분리되어 주어진다. 각각의 A[x]는 30,000을 넘�
 
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
+
+lt = 0
+rt = 1
+tot = a[0]
+cnt = 0
+
+while True:
+    if tot < m:
+        if rt < n:
+            tot += a[rt]
+            rt += 1
+        else:
+            break
+    elif tot == m:
+        cnt += 1
+        tot -= a[lt]
+        lt += 1
+    else:
+        tot -= a[lt]
+        lt += 1
+
+print(cnt)

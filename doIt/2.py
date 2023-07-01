6c7bd6429f6735ea0509d73bfa0d209c065ca57c
User <y55mun33@gmail.com>
2023-07-01 13:09:13 +0900

    풀이 완료

---
+++ doIt/2.py
@@ -0,0 +1,12 @@
+'''평균
+https://www.acmicpc.net/problem/1546
+
+'''
+
+n = int(input())
+scores = list(map(int, input().split()))
+m = max(scores)
+
+for i in range(n):
+     scores[i] = ( scores[i] /m)*100
+print(sum(scores) / n)

3128a28dd2577224afafe50b804b4e9725d382a2
User <y55mun33@gmail.com>
2023-07-01 21:48:47 +0900

    주석 추가

---
+++ doIt/6_2018.py
@@ -0,0 +1,23 @@
+''' 수들의 합 5
+https://www.acmicpc.net/problem/2018
+투포인터
+'''
+
+n = int(input())
+s = 1
+e = 1
+cnt = 1
+sum = 1
+
+while e != n:
+    if sum < n:
+        e += 1
+        sum += e
+    elif sum > n:
+        sum -= s
+        s += 1
+    else:
+        e += 1
+        sum += e
+        cnt += 1
+print(cnt)

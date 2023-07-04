83adbf4f8db52bfbc718298f8c29314161f2ad36
User <y55mun33@gmail.com>
2023-07-05 00:07:06 +0900

    시간 초과 발생

--- doIt/12_17298.py
+++ doIt/12_17298.py
@@ -9,10 +9,11 @@ ans = [0] * n
 a = list(map(int, input().split()))
 stack = []
 
-for i in range(n):
+stack.append(0)
+for i in range(1, n):
     while stack and a[stack[-1]] < a[i]:    # 스택이 비어있지 않고, 현재 수열이 스택 top 보다 클 경우
         ans[stack.pop()] = a[i]
     stack.append(i)
 
 
-print(*ans)
+print(ans)

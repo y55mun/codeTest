3bba5866bbd20c999a77b00fef30d5b250ba38a8
User <y55mun33@gmail.com>
2023-07-05 00:06:49 +0900

    시간 초과 발생

---
+++ doIt/12_17298.py
@@ -0,0 +1,18 @@
+""" 오큰수
+https://www.acmicpc.net/problem/17298
+"""
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+ans = [0] * n
+a = list(map(int, input().split()))
+stack = []
+
+for i in range(n):
+    while stack and a[stack[-1]] < a[i]:    # 스택이 비어있지 않고, 현재 수열이 스택 top 보다 클 경우
+        ans[stack.pop()] = a[i]
+    stack.append(i)
+
+
+print(*ans)

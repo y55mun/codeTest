0b7d1f247ac961e128bd5c05572b2e989f8292df
User <y55mun33@gmail.com>
2023-07-05 23:30:03 +0900

    풀이 완료

---
+++ doIt/14_11286.py
@@ -0,0 +1,20 @@
+"""절댓값 힙
+https://www.acmicpc.net/problem/11286
+"""
+from queue import PriorityQueue
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+queue = PriorityQueue()
+
+for i in range(n):
+    request = int(input())
+    if request == 0:
+        if queue.empty():
+            print('0')
+        else:
+            temp = queue.get()
+            print(str((temp[1])))
+    else:
+        queue.put((abs(request), request))

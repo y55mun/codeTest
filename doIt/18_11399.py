ef6e1038d58ed34df3fb55311f9c07507adfc758
User <y55mun33@gmail.com>
2023-07-12 23:10:02 +0900

    풀이 완료 - 주석 추가

--- doIt/18_11399.py
+++ doIt/18_11399.py
@@ -4,20 +4,21 @@ https://www.acmicpc.net/problem/11399
 
 n = int(input())
 a = list(map(int, input().split()))
-s = [0] * n
+s = [0] * n # 합 배열: 각 사람이 인출을 완료하는데 필요한 시간을 저장
 
 for i in range(1, n):
     insert_point = i
     insert_value = a[i]
-    for j in range(i-1, -1, -1):
+
+    for j in range(i-1, -1, -1):    # 현재 범위에서 삽입 위치 찾기
         if a[j] < a[i]:
             insert_point = j+1
             break
         if j == 0:
             insert_point = 0
-    for j in range(i, insert_point, -1):
-        a[j] = a[j-1]
-    a[insert_point] = insert_value
+    for j in range(i, insert_point, -1):    # 삽입을 위해 삽입 위치에서 i까지 데이터를 한 칸씩 뒤로 밀기기
+       a[j] = a[j-1]
+    a[insert_point] = insert_value  # 삽입 위치에 현재 데이터 저장
 
 s[0] = a[0]
 

f453c0462c24ffbb7eb57724056973c6a701e3b2
User <y55mun33@gmail.com>
2023-07-22 11:47:48 +0900

    변수 초기화

--- doIt/25_13023.py
+++ doIt/25_13023.py
@@ -3,6 +3,10 @@ https://www.acmicpc.net/problem/13023
 """
 
 import sys
+sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
+arrive = False
+a = [[] for _ in range(n+1)]
+visited = [False] * (n+1)

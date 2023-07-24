78ac1d5863563dba671c3468201db71f31b410b9
User <y55mun33@gmail.com>
2023-07-24 22:04:25 +0900

    ing

--- doIt/25_13023.py
+++ doIt/25_13023.py
@@ -4,14 +4,13 @@ https://www.acmicpc.net/problem/13023
 
 import sys
 sys.setrecursionlimit(10000)
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
+input = sys.stdin.readline()
+n,m = map(int,input().split())
 arrive = False
 a = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
 
-def DFS(now, depth):
+def dfs(now, depth):
     global arrive
     if depth == 5:
         arrive = True
@@ -19,19 +18,5 @@ def DFS(now, depth):
     visited[now] = True
     for i in a[now]:
         if not visited[i]:
-            DFS(i, depth+1)
+            dfs(now, depth+1)
     visited[now] = False
-
-for _ in range(m):
-    s, e = map(int, input().split())
-    a[s].append(e)
-    a[e].append(s)
-
-for i in range(n):
-    DFS(i, 1)
-    if arrive:
-        break
-    if arrive:
-        print(1)
-    else:
-        print(0)

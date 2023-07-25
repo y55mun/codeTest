240a919a35917a23ff2315528f21df6bd11ccbf1
User <y55mun33@gmail.com>
2023-07-25 20:58:41 +0900

    시간 초과 발생

--- doIt/25_13023.py
+++ doIt/25_13023.py
@@ -4,8 +4,8 @@ https://www.acmicpc.net/problem/13023
 
 import sys
 sys.setrecursionlimit(10000)
-input = sys.stdin.readline()
-n,m = map(int,input().split())
+input = sys.stdin.readline
+n, m = map(int, input().split())
 arrive = False
 a = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
@@ -20,3 +20,17 @@ def dfs(now, depth):
         if not visited[i]:
             dfs(now, depth+1)
     visited[now] = False
+
+for _ in range(m):
+    s,e = map(int, input().split())
+    a[s].append(e)
+    a[e].append(s)
+
+for i in range(n):
+    dfs(i, 1)
+    if arrive:
+        break
+if arrive:
+    print(1)
+else:
+    print(0)

3c2cf627333bc201431933adcab8a0ca48d4856e
User <y55mun33@gmail.com>
2023-07-27 21:05:12 +0900

    .

--- doIt/25_13023.py
+++ doIt/25_13023.py
@@ -5,6 +5,7 @@ https://www.acmicpc.net/problem/13023
 import sys
 sys.setrecursionlimit(10000)
 input = sys.stdin.readline
+
 n, m = map(int, input().split())
 arrive = False  # 정답
 a = [[] for _ in range(n+1)]

a14c75ae85030f3a87f42348b2177fd0becf4339
User <y55mun33@gmail.com>
2023-08-02 22:56:02 +0900

    풀이 완료 - enumerate 를 이용한 풀이, 주석 추가

--- programmers/lv1/24.py
+++ programmers/lv1/24.py
@@ -12,6 +12,7 @@ def solution(answers):
     student2 = [2, 1, 2, 3, 2, 4, 2, 5]
     student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
+    # 각 학생 별 정답과 비교 후 정답 갯수 카운트
     for i in range(len(answers)):
         if answers[i] == student1[i % 5]:
             score[0] += 1
@@ -20,6 +21,7 @@ def solution(answers):
         if answers[i] == student3[i % 10]:
             score[2] += 1
 
+    # enumerate 함수를 이용해 점수에 해당하는 인덱스, 즉, 학생 구하기
     for idx, num in enumerate(score):
         if num == max(score):
             answer.append(idx + 1)

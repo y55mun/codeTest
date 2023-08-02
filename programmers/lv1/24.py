218036a6c203f8bf3f87d77aef8df0291eccc163
User <y55mun33@gmail.com>
2023-08-02 22:49:13 +0900

    풀이 완료 - 완전 탐색

--- programmers/lv1/24.py
+++ programmers/lv1/24.py
@@ -3,3 +3,26 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/42840
 
 """
+
+
+def solution(answers):
+    answer = []
+
+    s1 = [1, 2, 3, 4, 5]
+    s2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
+
+    for i in range(len(answers)):
+        if answers[i] == s1[i % 5]:
+            score[0] += 1
+        if answers[i] == s2[i % 8]:
+            score[1] += 1
+        if answers[i] == s3[i % 10]:
+            score[2] += 1
+
+    for i in range(len(score)):
+        if score[i] == max(score):
+            answer.append(i + 1)
+
+    return sorted(answer)

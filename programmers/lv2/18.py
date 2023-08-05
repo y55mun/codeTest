7f2f55aa3b1c2d0243e2c13132b531b2df886fbf
User <y55mun33@gmail.com>
2023-08-05 15:59:44 +0900

    풀이 완료 - 주석 추가

--- programmers/lv2/18.py
+++ programmers/lv2/18.py
@@ -8,10 +8,11 @@ def solution(k, dungeons):
     answer = -1
 
     for permute in permutations(dungeons, len(dungeons)):
-        hp = k
-        count = 0
+        hp = k  # 최대 피로도
+        count = 0   # 돌 수 있는 던전의 수
 
         for pm in permute:
+
             if hp >= pm[0]:
                 hp -= pm[1]
                 count += 1

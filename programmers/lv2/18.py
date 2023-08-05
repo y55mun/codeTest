0825533831a1c24c78deed006cc6cd93aa486f66
User <y55mun33@gmail.com>
2023-08-05 15:37:20 +0900

    풀이 완료

--- programmers/lv2/18.py
+++ programmers/lv2/18.py
@@ -2,3 +2,20 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/87946
 """
 
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = -1
+
+    for permute in permutations(dungeons, len(dungeons)):
+        hp = k
+        count = 0
+
+        for pm in permute:
+            if hp >= pm[0]:
+                hp -= pm[1]
+                count += 1
+            if count > answer:
+                answer = count
+
+    return answer

c434aef6a6350562fde536fc9b463f166a9f5d00
User <y55mun33@gmail.com>
2023-08-06 12:15:08 +0900

    다른 풀이

--- programmers/lv2/19.py
+++ programmers/lv2/19.py
@@ -1,38 +1,31 @@
 """ 전력망을 둘로 나누기
 https://school.programmers.co.kr/learn/courses/30/lessons/86971
 """
-
 from collections import deque
 
 
 def solution(n, wires):
-    answer = 100000
+    tree = [[] for _ in range(n + 1)]
 
-    tree = [[] for _ in range(n + 1)]  # 0번부터 노드가 시작되는 것이 아니기 때문에 +1
-
-    for a, b in wires:  # 양방향 이므로 양쪽 모두 추가
+    for a, b in wires:
         tree[a].append(b)
         tree[b].append(a)
+    visited = [False] * (n + 1)
+    child = [0] * (n + 1)
 
-    for node1, node2 in wires:
-        visited = [False for _ in range(n + 1)]
+    def dfs(node):  # 각자의 자식의 개수 구하기
+        visited[node] = True
 
-        q = deque()
-        q.append(node1)
-        result = 1
-        visited[node1] = True  # 여기서 부터 시작!
-        visited[node2] = True  # 한 방향만 순회 할 것 이므로, 다른 방향도 방문했다고 체크
+        for next in tree[node]:
+            if not visited[next]:
+                child[node] += dfs(next) + child[next]
 
-        while q:
-            node = q.popleft()
-            for ele in tree[node]:
-                if not visited[ele]:
-                    result += 1
-                    visited[ele] = True
-                    q.append(ele)
-        min_value = min(result, n - result)
-        max_value = n - min_value
+        return 1
 
-        if answer > max_value - min_value:
-            answer = max_value - min_value
-    return answer
+    dfs(1)
+    result = n
+
+    for c in child:
+        result = min(result, abs(n - 2 * (c + 1)))
+
+    return result

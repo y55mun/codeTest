8a9ab91145430a21284661fc8c01e1757a5fbe48
User <y55mun33@gmail.com>
2023-08-08 20:18:29 +0900

    풀이 완료 - 중복순열

---
+++ programmers/lv2/20.py
@@ -0,0 +1,19 @@
+""" 모음사전
+https://school.programmers.co.kr/learn/courses/30/lessons/84512
+"""
+
+from itertools import product
+
+
+def solution(word):
+    answer = 0
+    char = ['A', 'E', 'I', 'O', 'U']
+    tmp = []
+
+    for i in range(1, 6):
+        for j in product(char, repeat=i):
+            tmp.append(''.join(j))
+
+    tmp.sort()
+
+    return tmp.index(word) + 1

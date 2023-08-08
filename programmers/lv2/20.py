02d5bc7fc5e5fe3486f52d8b7a86f821006be093
User <y55mun33@gmail.com>
2023-08-08 20:22:27 +0900

    풀이 완료

--- programmers/lv2/20.py
+++ programmers/lv2/20.py
@@ -2,18 +2,14 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/84512
 """
 
-from itertools import product
-
-
 def solution(word):
     answer = 0
-    char = ['A', 'E', 'I', 'O', 'U']
-    tmp = []
+    dic = ['A', 'E', 'I', 'O', 'U']
+    li = [5 ** i for i in range(len(dic))]
 
-    for i in range(1, 6):
-        for j in product(char, repeat=i):
-            tmp.append(''.join(j))
-
-    tmp.sort()
-
-    return tmp.index(word) + 1
+    for i in range(len(word) - 1, -1, -1):
+        idx = dic.index(word[i])
+        for j in range(5 - i):
+            answer += li[j] * idx
+        answer += 1
+    return answer

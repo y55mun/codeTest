e27f4bd24292f750f5937db27d2e05b2b7e586c2
User <y55mun33@gmail.com>
2023-08-10 23:30:16 +0900

    풀이 완료 - 주석 추가

--- programmers/lv2/21.py
+++ programmers/lv2/21.py
@@ -3,10 +3,12 @@ https://school.programmers.co.kr/learn/courses/30/lessons/43165
 """
 
 def solution(numbers, target):
-    sup = [0]
+    sup = [0]   # 모든 계산 결과를 담자
 
     for i in numbers:
         sub = []
+
+        # 자손 노드
         for j in sup:
             sub.append(j + i)
             sub.append(j - i)

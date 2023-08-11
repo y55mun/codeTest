7014788cf1420c647f1f08d0bd46efdeb194cfa7
User <y55mun33@gmail.com>
2023-08-11 22:04:00 +0900

    풀이완료

--- programmers/lv2/22.py
+++ programmers/lv2/22.py
@@ -4,19 +4,25 @@ https://school.programmers.co.kr/learn/courses/30/lessons/43162
 
 """
 1. 아이디어
-- BFS로 풀기
+- DFS로 풀기
 
 """
 
 def solution(n, computers):
     answer = 0
-    connect = [[0] * n for _ in range(n)]
 
-    for j in range(n):
-        for i in range(n):
-            if computers[j][i] == 1 or computers[i][j] == 1:
-                if i != j:
-                    print(i, j)
-                    answer += 1
+    def DFS(i):
+        visited[i] = 1
+
+        for com in range(n):
+            if computers[i][com] and not visited[com]:
+                DFS(com)
+
+    visited = [0 for i in range(len(computers))]
+
+    for i in range(n):
+        if not visited[i]:
+            DFS(i)
+            answer += 1
 
     return answer

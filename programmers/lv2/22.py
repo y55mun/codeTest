1510422b976874b1554fb2647965bb35340a8dec
User <y55mun33@gmail.com>
2023-08-11 22:10:19 +0900

    풀이완료 - 다른풀이 추가

--- programmers/lv2/22.py
+++ programmers/lv2/22.py
@@ -26,3 +26,30 @@ def solution(n, computers):
             answer += 1
 
     return answer
+
+
+""" 다른풀이(BFS)
+from collections import deque
+
+def solution(n, computers):            
+    
+    def BFS(i):
+        q = deque()
+        q.append(i)
+        while q:
+            i = q.popleft()
+            visited[i] = 1
+            for a in range(n):
+                if computers[i][a] and not visited[a]:
+                     q.append(a)
+                
+    answer = 0
+    visited = [0 for i in range(len(computers))]
+    for i in range(n):
+        if not visited[i]:
+            BFS(i)
+            answer += 1
+        
+    return answer
+
+"""

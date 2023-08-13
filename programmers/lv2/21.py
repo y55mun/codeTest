97bad6ffbec447a981199892709e297edef230a3
User <y55mun33@gmail.com>
2023-08-13 20:40:29 +0900

    풀이 완료 - 다른 풀이 추가

--- programmers/lv2/21.py
+++ programmers/lv2/21.py
@@ -16,3 +16,24 @@ def solution(numbers, target):
         sup = sub
 
     return sup.count(target)
+
+"""
+a = 0
+
+def dfs(i, result, numbers, target) :
+    global a
+    if i == len(numbers) :
+        if result == target : 
+            a+=1
+            return
+    else :
+        dfs(i+1, result+numbers[i], numbers, target)
+        dfs(i+1, result-numbers[i], numbers, target)
+
+def solution(numbers, target):
+    result = 0
+    dfs(0, result, numbers, target)
+    
+    return a
+
+"""

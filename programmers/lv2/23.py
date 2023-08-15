e33560e304c80fe0f9fefa9275ef463a4c201b05
User <y55mun33@gmail.com>
2023-08-15 22:58:58 +0900

    풀이 완료

--- programmers/lv2/23.py
+++ programmers/lv2/23.py
@@ -9,20 +9,14 @@ https://school.programmers.co.kr/learn/courses/30/lessons/1844
 """
 from collections import deque
 
-
-def solution(graph):
-    # 행,열 길이 정리
-    row = len(graph)
-    col = len(graph[0])
-
-    # BFS 함수
+def solution(maps):
     def bfs(y, x):
         # 큐 생성 후 초기값 추가
         q = deque()
         q.append((y, x))
 
         while q:
-            # ex(행),ey(열) 좌표 받기
+            # x(행),y(열) 좌표 받기
             ey, ex = q.popleft()
 
             # 방향 벡터(상,하,좌,우)로 탐색
@@ -32,27 +26,30 @@ def solution(graph):
             for i in range(4):
                 nx = ex + dx[i]
                 ny = ey + dy[i]
+
                 # 범위를 벗어나는 경우 무시
-                if nx < 0 or ny < 0 or nx >= len(graph[0]) or ny >= len(graph):
+                if nx < 0 or ny < 0 or nx >= len(maps[0]) or ny >= len(maps):
                     continue
+
                 # 벽인 경우 무시
-                if graph[ey][ex] == 0:
+                if maps[ey][ex] == 0:
                     continue
+
                 # 방문한 적이 없는 경우에만
-                if graph[ny][nx] == 1:
+                if maps[ny][nx] == 1:
                     # 이전 좌표값 +1
-                    graph[ny][nx] = graph[ey][ex] + 1
+                    maps[ny][nx] = maps[ey][ex] + 1
 
                     # 큐에 새로 추가
                     q.append((ny, nx))
 
         # 상대 진영에 방문한 적이 없다면
-        if graph[len(graph) - 1][len(graph[0]) - 1] == 1:
+        if maps[len(maps) - 1][len(maps[0]) - 1] == 1:
             return -1
 
-        # 상대 진영에 방문했다면 기록 반환
+        # 상대 진영에 방문했다면 기록 반환 (그래프의 가장 오른쪽 아래 반환)
         else:
-            return graph[len(graph) - 1][len(graph[0]) - 1]
+            return maps[len(maps) - 1][len(maps[0]) - 1]
 
     answer = bfs(0, 0)
     return answer

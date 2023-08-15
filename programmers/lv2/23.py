cf0cb6f5cfc739ddfc1257c4fb1d3c2f85402133
User <y55mun33@gmail.com>
2023-08-15 22:24:55 +0900

    풀이 완료

--- programmers/lv2/23.py
+++ programmers/lv2/23.py
@@ -7,41 +7,42 @@ https://school.programmers.co.kr/learn/courses/30/lessons/1844
 
 
 """
-
 from collections import deque
-import sys
 
-
-def solution(maps):
-    answer = 0
-
-    dy = [0, 1, 0, -1]
-    dx = [1, 0, -1, 0]
-
+def solution(graph):
+    # BFS 함수
     def bfs(y, x):
+
         q = deque()
         q.append((y, x))
 
         while q:
-            ey, ex = q.popleft()
+            y, x = q.popleft()
 
-            for k in range(4):
-                ny = ey + dy[k]
-                nx = ex + dx[k]
+            dy = [0, 1, 0, -1]
+            dx = [1, 0, -1, 0]
 
-                if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]):
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if nx < 0 or ny < 0 or nx >= len(graph[0]) or ny >= len(graph):
                     continue
 
-                # 벽이면 무시
-                if maps[ny][nx] == 0: continue
+                if graph[y][x] == 0:
+                    continue
 
-                if maps[ny][nx] == 1:
-                    maps[ny][nx] = maps[ey][ex] + 1
+                if graph[ny][nx] == 1:
+                    graph[ny][nx] = graph[y][x] + 1
+
                     q.append((ny, nx))
 
-        # 상대 팀 진영(제일 오른쪽 아래 칸)까지의 거리 반환
-        return maps[len(maps) - 1][len(maps[0]) - 1]
+        if graph[len(graph) - 1][len(graph[0]) - 1] == 1:
+            return -1
+
+
+        else:
+            return graph[len(graph) - 1][len(graph[0]) - 1]
 
     answer = bfs(0, 0)
-
-    return -1 if answer == 1 else answer
+    return answer

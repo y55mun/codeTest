ed3112a73e61bfd680aa62b1cfc8237749f1ea60
User <y55mun33@gmail.com>
2023-08-22 23:39:54 +0900

    길찾기는 구현 완료

--- programmers/lv3/1.py
+++ programmers/lv3/1.py
@@ -13,4 +13,19 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
     visited = [[0] * MAX for _ in range(MAX)]
     visited[characterX * 2][characterY * 2] = 1
 
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    while q:
+        x, y = q.popleft()
+
+        if x == itemX * 2 and y == itemY*2:
+            answer = (visited[x][y]-1) // 2
+            break
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if visited[nx][ny] == 0 and field[nx][ny] == 1:
+                q.append([nx, ny])
+                visited[nx][ny] = visited[x][y] + 1
+
     return answer

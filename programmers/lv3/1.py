5df94801398924fb1c4d8301a2bbc5a1dd07311e
User <y55mun33@gmail.com>
2023-08-22 23:35:20 +0900

    길찾기 로직 구현 중

--- programmers/lv3/1.py
+++ programmers/lv3/1.py
@@ -1,3 +1,15 @@
 """ 아이템 줍기
 https://school.programmers.co.kr/learn/courses/30/lessons/87694
 """
+from collections import deque
+
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    answer = 0
+    MAX = 102
+
+    # 길찾기
+    q = deque()
+    q.append([characterX * 2, characterY * 2])
+    visited = [[0] * MAX for _ in range(MAX)]
+
+    return answer

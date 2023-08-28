bb0c3194c0ed802935649fd4cb7bfdfeb048a196
User <y55mun33@gmail.com>
2023-08-28 21:38:12 +0900

    풀이 완료-dfs

--- programmers/lv3/2.py
+++ programmers/lv3/2.py
@@ -1,36 +1,27 @@
 """ 여행경로
 https://school.programmers.co.kr/learn/courses/30/lessons/43164
 """
-
 from collections import deque
 
+
 def solution(tickets):
-    answer = deque()
+    answer = []
     ticket_dict = {}
 
-    for s, d in tickets:
-        if s not in ticket_dict.keys():
-            ticket_dict[s] = [d]
+    for (start, end) in tickets:
+        if start in ticket_dict:
+            ticket_dict[start].append(end)
         else:
-            ticket_dict[s].append(d)
+            ticket_dict[start] = [end]
 
-    keys = ticket_dict.keys()
-    for i in keys:
-        ticket_dict[i].sort(reverse=True)
-
-    stack = ['ICN']
-    while stack:
-        dt = stack[-1]
-        if dt not in keys:
-            answer.appendleft(stack.pop())
-        elif not ticket_dict[dt]:
-            answer.appendleft(stack.pop())
+    for t in ticket_dict.keys():
+        ticket_dict[t].sort(reverse=True)
+    st = ["ICN"]
+    while st:
+        top = st[-1]
+        if (top not in ticket_dict) or (not ticket_dict[top]):
+            answer.append(st.pop())
         else:
-            stack.append(ticket_dict[dt].pop())
-    return list(answer)
+            st.append(ticket_dict[top].pop())
 
-    for i in range(len(tickets)):  # 가로
-        for j in range(len(tickets[i])):
-            print(tickets[i][0])
-
-    return answer
+    return answer[::-1]

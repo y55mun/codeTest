617a8a12892cf77367f1413d3b2c841d93e4a59c
User <y55mun33@gmail.com>
2023-08-29 23:05:48 +0900

    런타임 에러

--- doIt/23_11724.py
+++ doIt/23_11724.py
@@ -1,3 +1,29 @@
 """연결 요소의 개수
 https://www.acmicpc.net/problem/11724
 """
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+
+def dfs(v):
+    visited[v] = True
+    for i in a[v]:
+        if not visited[v]:
+            dfs(i)
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    a[s].append[e]
+    a[e].append[s]
+
+count = 0
+for i in range(1, n+1):
+    if not visited[i]:
+        count += 1
+        dfs(i)
+
+print(i)

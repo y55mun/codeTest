77a5a962743d4e6c377f8d2f3cd59a2e889ef556
User <y55mun33@gmail.com>
2023-09-03 11:01:14 +0900

    풀이 완료

--- doIt/25_13023.py
+++ doIt/25_13023.py
@@ -17,4 +17,29 @@ def dfs(now,depth):
     if depth == 5:
         arrive = True
         return
+
+    # 방문 리스트에 현재 노드 방문 기록 저장
     visited[now] = True
+
+    # 방문 노드의 연결 노드 중 방문하지 않은 노드로 DFS 실행
+    for i in a[now]:
+        if not visited[i]:
+            dfs(i, depth+1)
+    visited[now] = False
+
+for _ in range(m):
+    s,e = map(int, input().split())
+    a[s].append(e)
+    a[e].append(s)
+
+for i in range(n):
+    # 노드마다 dfs 실행
+    dfs(i,1)
+
+    if arrive:
+        break
+
+if arrive:
+    print(1)
+else:
+    print(0)

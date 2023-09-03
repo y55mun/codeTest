555de1d0b5ff18a0bc867e2546c77cd45eae15a2
User <y55mun33@gmail.com>
2023-09-03 10:51:12 +0900

    구현중

--- doIt/25_13023.py
+++ doIt/25_13023.py
@@ -1,5 +1,20 @@
 """ABCDE
 https://www.acmicpc.net/problem/13023
+
+1. 아이디어
+- 노드에 연결되어 있는 값들을 넣고 조건 확인
+-
 """
 
 import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arrive = False
+a = [[] for _ in range(n+1) ]
+visited = [[False]*(n+1)]
+
+def dfs(now,depth):
+    if depth == 5:
+        arrive = True
+        return
+    visited[now] = True

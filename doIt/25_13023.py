632418e2fde75978638362ae80251202e38a2f7b
User <y55mun33@gmail.com>
2023-09-03 11:06:55 +0900

    global 변수 추가-arrive

--- doIt/25_13023.py
+++ doIt/25_13023.py
@@ -4,13 +4,15 @@ https://www.acmicpc.net/problem/13023
 """
 
 import sys
+sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 n, m = map(int, input().split())
 arrive = False
-a = [[] for _ in range(n+1) ]
+a = [[] for _ in range(n+1)]
 visited = [[False]*(n+1)]
 
 def dfs(now,depth):
+    global arrive
     if depth == 5:
         arrive = True
         return

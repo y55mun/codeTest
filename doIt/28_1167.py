4ff86f04c33dff28095bbd69aac9daf396f61d50
User <y55mun33@gmail.com>
2023-09-07 21:29:00 +0900

    기본 로직 구현

--- doIt/28_1167.py
+++ doIt/28_1167.py
@@ -2,7 +2,26 @@
 https://www.acmicpc.net/problem/1167
 
 """
+from collections import deque
 
 import sys
 input = sys.stdin.readline
-v = int(input())
+n = int(input())
+a = [[] for _ in range(n+1)]
+
+for _ in range(n):
+    data = list(map(int, input().split()))
+    index = 0
+    s = data[index]
+    index += 1
+    while True:
+        e = data[index]
+        if e == -1:
+            break
+        v = data[index+1]
+        a[s].append((e,v))
+        index += 2
+
+distance = [0] * (n+1)
+visited = [False] * (n+1)
+

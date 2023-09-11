509a91e2698f1aa190bfeb9da03ce106ab437377
User <y55mun33@gmail.com>
2023-09-11 16:59:57 +0900

    .

---
+++ BAEKJOON/1_2609.py
@@ -0,0 +1,16 @@
+""" 최대공약수와 최소공배수
+https://www.acmicpc.net/problem/2609
+"""
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+def gcd(n,m):
+    if n%m == 0:
+        return m
+    else:
+        return gcd(m, n%m)
+
+print(gcd(n,m))
+print((n*m)// gcd(m,n))

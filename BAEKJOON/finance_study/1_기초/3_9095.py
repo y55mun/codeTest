64c0647260bda62e4b669a98695357443c74752e
User <y55mun33@gmail.com>
2023-09-13 22:05:18 +0900

    풀이 완료 - for 문 범위 변수 수정, 각각의 값 일때마다 print 가 아닌 dp 로 수정

--- BAEKJOON/finance_study/1_기초/3_9095.py
+++ BAEKJOON/finance_study/1_기초/3_9095.py
@@ -20,12 +20,12 @@ for _ in range(t):
     dp = [0] * (n+1)
 
     for i in range(1, n+1):
-        if n==1:
-            print(1)
-        elif n==2:
-            print(2)
-        elif n==3:
-            print(4)
+        if i==1:
+            dp[i] = 1
+        elif i==2:
+            dp[i] = 2
+        elif i==3:
+            dp[i] = 4
         else:
             dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
     print(dp[n])

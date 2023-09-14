784ce5506a13bc3d43944af167f0d8dd020d6eff
User <y55mun33@gmail.com>
2023-09-15 00:15:48 +0900

    시간 초과

--- BAEKJOON/finance_study/1_기초/4_1748.py
+++ BAEKJOON/finance_study/1_기초/4_1748.py
@@ -1,6 +1,10 @@
 """ 수 이어 쓰기 1
 https://www.acmicpc.net/problem/1748
 """
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
 n = int(input())
 ans = str(1)
 for i in range(2, n+1):

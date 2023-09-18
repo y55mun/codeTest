5d16b4d22872c2ccb927aeee30a3885ad46c234e
User <y55mun33@gmail.com>
2023-09-18 22:00:43 +0900

    리스트 컴프리헨션

--- BAEKJOON/finance_study/1_기초/5_2309.py
+++ BAEKJOON/finance_study/1_기초/5_2309.py
@@ -1,3 +1,8 @@
 """ 일곱난쟁이
 https://www.acmicpc.net/problem/2309
 """
+
+# seven_list = list(map(int, input().rstrip()))
+seven_list = [int(input()) for _ in range(9)]   # 리스트 컴프리핸션 사용법
+
+print(seven_list)

32cb1fbde08c1c67183bd24afaa7721b528a2b01
User <y55mun33@gmail.com>
2023-09-19 21:56:38 +0900

    주석 추가

--- BAEKJOON/finance_study/1_기초/5_2309.py
+++ BAEKJOON/finance_study/1_기초/5_2309.py
@@ -6,8 +6,8 @@ import itertools
 
 seven_list = [int(input()) for _ in range(9)]   # 리스트 컴프리핸션 사용법
 
-for i in itertools.combinations(seven_list, 7):
-    if sum(i) == 100:
-        for j in sorted(i):
-            print(j)
+for i in itertools.combinations(seven_list, 7): # 7명을 중복 없이 뽑음
+    if sum(i) == 100:   # 합이 100이면
+        for j in sorted(i): # 7명을 오름차순으로 정렬
+            print(j)    # 출력
         break

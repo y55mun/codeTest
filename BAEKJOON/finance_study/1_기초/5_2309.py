b1e56e4d900caf2ac263231027f0e79540e28987
User <y55mun33@gmail.com>
2023-09-19 20:53:57 +0900

    풀이완료

--- BAEKJOON/finance_study/1_기초/5_2309.py
+++ BAEKJOON/finance_study/1_기초/5_2309.py
@@ -2,7 +2,12 @@
 https://www.acmicpc.net/problem/2309
 """
 
-# seven_list = list(map(int, input().rstrip()))
+import itertools
+
 seven_list = [int(input()) for _ in range(9)]   # 리스트 컴프리핸션 사용법
 
-print(seven_list)
+for i in itertools.combinations(seven_list, 7):
+    if sum(i) == 100:
+        for j in sorted(i):
+            print(j)
+        break

fbd3b173818dcffd8f8493c405926bb684ec80ac
User <y55mun33@gmail.com>
2023-09-25 23:55:21 +0900

    풀이 완성

--- BAEKJOON/finance_study/1_기초/6_3085.py
+++ BAEKJOON/finance_study/1_기초/6_3085.py
@@ -24,3 +24,36 @@ input = sys.stdin.readline
 n = int(input())
 board = [list(input()) for _ in range(n)]
 maxCnt = 0
+
+def check():
+    global maxCnt
+    for i in range(n):
+        cnt = 1
+        for j in range(1, n):
+            if board[i][j] == board[i][j-1]:
+                cnt += 1
+                maxCnt = max(cnt, maxCnt)
+            else:
+                cnt = 1
+        cnt = 1
+        for j in range(1, n):
+            if board[j][i] == board[j-1][i]:
+                cnt += 1
+                maxCnt = max(cnt, maxCnt)
+            else:
+                cnt = 1
+
+for i in range(n):
+    for j in range(n):
+        if j+1 < n:
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+            check()
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+
+        if i+1 < n:
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
+            check()
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
+
+
+print(maxCnt)

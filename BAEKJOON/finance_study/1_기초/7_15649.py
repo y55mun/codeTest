5db8a4e1d2c62ccf21e0b7ab2ef56dde9dc72b49
User <y55mun33@gmail.com>
2023-09-27 01:28:31 +0900

    완성!

--- BAEKJOON/finance_study/1_기초/7_15649.py
+++ BAEKJOON/finance_study/1_기초/7_15649.py
@@ -8,7 +8,7 @@ from itertools import permutations
 n, m = map(int, input().split())
 list_n = [i for i in range(1, n+1)]
 
-answers = [permutations(list_n, m)]
+answers = list(permutations(list_n, m))
 
 for i in answers:
     print(' '.join(map(str, i)))

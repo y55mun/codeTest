2201d18d279461b759f34cb2b0fa2210adc1f650
User <y55mun33@gmail.com>
2023-09-26 23:54:24 +0900

    .

--- BAEKJOON/finance_study/1_기초/6_3085.py
+++ BAEKJOON/finance_study/1_기초/6_3085.py
@@ -28,14 +28,14 @@ maxCnt = 0
 def check():
     global maxCnt
     for i in range(n):
-        cnt = 1
+        cnt = 1     # 행을 검사
         for j in range(1, n):
             if board[i][j] == board[i][j-1]:
                 cnt += 1
                 maxCnt = max(cnt, maxCnt)
             else:
                 cnt = 1
-        cnt = 1
+        cnt = 1     # 열을 검사
         for j in range(1, n):
             if board[j][i] == board[j-1][i]:
                 cnt += 1
@@ -45,11 +45,13 @@ def check():
 
 for i in range(n):
     for j in range(n):
+        # 오른쪽과 바꿈
         if j+1 < n:
             board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
             check()
             board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
 
+        # 아래쪽과 바꿈
         if i+1 < n:
             board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
             check()

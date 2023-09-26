3e29e11543736e1509c85b68ab28dca03ae491a3
User <y55mun33@gmail.com>
2023-09-27 01:25:14 +0900

    오류 발생- [(1,), (2,), (3,)] 이런식으로 출력 됨

--- BAEKJOON/finance_study/1_기초/7_15649.py
+++ BAEKJOON/finance_study/1_기초/7_15649.py
@@ -5,7 +5,7 @@ import sys
 input = sys.stdin.readline
 from itertools import permutations
 
-n, m = list(map(int, input().split()))
+n, m = map(int, input().split())
+list_n = [i for i in range(1, n+1)]
 
-
-print(list(permutations(n,m)))
+print(list(permutations(list_n, m)), end='')

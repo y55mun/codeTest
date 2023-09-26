a4fc7cac04e29f866ad226bb7707d73d2ca0cb37
User <y55mun33@gmail.com>
2023-09-27 01:24:13 +0900

    오류 발생- int형은 iterable 하지 않음

--- BAEKJOON/finance_study/1_기초/7_15649.py
+++ BAEKJOON/finance_study/1_기초/7_15649.py
@@ -1,4 +1,11 @@
 """ N과 M (1)
 https://www.acmicpc.net/problem/15649
 """
+import sys
+input = sys.stdin.readline
+from itertools import permutations
 
+n, m = list(map(int, input().split()))
+
+
+print(list(permutations(n,m)))

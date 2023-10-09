7d41f0077976301fa942a9fd811d1e3b05d0857d
User <y55mun33@gmail.com>
2023-10-09 11:15:23 +0900

    풀이 완료

---
+++ BAEKJOON/finance_study/1_기초/8_3187.py
@@ -0,0 +1,53 @@
+"""
+https://www.acmicpc.net/problem/3187
+"""
+
+import sys
+input = sys.stdin.readline
+
+r, c = map(int, input().split())
+garden = [list(input()) for _ in range(r)]
+
+k, v = 0, 0
+
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+def bfs(y, x):
+    a, b = 0, 0
+    q = [(y, x)]
+
+    while q:
+        y, x = q.pop()
+
+        if garden[y][x] == "v":
+            b += 1
+
+        if garden[y][x] == "k":
+            a += 1
+
+        garden[y][x] = "#"
+
+        for k in range(4):
+            ny = y + dy[k]
+            nx = x + dx[k]
+
+            if 0<= ny < r and 0<=nx < c:
+                if garden[ny][nx] != "#":
+                    q.append((ny, nx))
+
+    if a <= b:
+        a = 0
+    else:
+        b = 0
+    return a,b
+
+
+for j in range(r):  # 세로
+    for i in range(c):  # 가로
+        if garden[j][i] == "v" or garden[j][i] == "k":
+            a, b = bfs(j, i)
+            k += a
+            v += b
+
+print(k, v)

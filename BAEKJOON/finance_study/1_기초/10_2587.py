c9c81ee4b805f1eecb7141e96b6941628862d895
User <y55mun33@gmail.com>
2023-10-22 22:48:54 +0900

    .

--- BAEKJOON/finance_study/1_기초/10_2587.py
+++ BAEKJOON/finance_study/1_기초/10_2587.py
@@ -5,7 +5,6 @@ https://www.acmicpc.net/problem/2587
 # list_num = [list(map(int, input())) for _ in range(5)]  # [[1, 0], [4, 0], [3, 0], [6, 0], [3, 0]]
 # list_num = [list(input()) for _ in range(5)]      # [['1', '0'], ['4', '0'], ['3', '0'], ['6', '0'], ['3', '0']]
 list_num = [int(input()) for _ in range(5)]     # [10, 40, 30, 60, 30]
-# print(list_num)
 
 print(int(sum(list_num) / 5))   # 평균
 

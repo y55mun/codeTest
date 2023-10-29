3c49432c9960463cbae5c5170b3a34772a322c9b
User <y55mun33@gmail.com>
2023-10-29 23:38:20 +0900

    .

--- BAEKJOON/finance_study/13_20055.py
+++ BAEKJOON/finance_study/13_20055.py
@@ -17,6 +17,7 @@ while 1:
     belt.rotate(1)
     robot.rotate(1)
     robot[-1] = 0  # 로봇이 내려가는 부분이니 0
+
     if sum(robot):  # 로봇이 존재하면
         for i in range(n - 2, -1, -1):  # 로봇 내려가는 부분 인덱스 i-1 이므로 그 전인 i-2부터
             if robot[i] == 1 and robot[i + 1] == 0 and belt[i + 1] >= 1:

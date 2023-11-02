89ed078cf766cea5e2ad16716bc5cdaa0cd87db5
User <y55mun33@gmail.com>
2023-11-02 21:37:46 +0900

    알고리즘

--- BAEKJOON/finance_study/13_20055.py
+++ BAEKJOON/finance_study/13_20055.py
@@ -1,36 +1,14 @@
 """ 컨베이어 벨트 위의 로봇
 https://www.acmicpc.net/problem/20055
 
+
+- 회전: deque 사용
+- n개의 벨트만 로봇이 존재 => n 길이의 로봇 유무를 저장하는 배열 생성
+
+1. 벨트 회전 => deque rotate
+2. 로봇 이동
+    - 가장 먼저 올라간 로봇부터 앞으로 이동
+    - 다음 칸: 내구도 >= 1 and 로봇 X and 현재 로봇이 존재할때
+3. 로봇 올리기: 올리는 위치에 내구도 != 0 and 로봇이 처음에 존재하지 X
+4. 중지: count(내구도 == 0) >= k
 """
-
-import sys
-
-input = sys.stdin.readline
-from collections import deque
-
-n, k = map(int, input().split())
-belt = deque(list(map(int, input().split())))
-robot = deque([0] * n)
-res = 0
-
-while 1:
-    belt.rotate(1)
-    robot.rotate(1)
-    robot[-1] = 0  # 로봇이 내려가는 부분이니 0
-
-    if sum(robot):  # 로봇이 존재하면
-        for i in range(n - 2, -1, -1):  # 로봇 내려가는 부분 인덱스 i-1 이므로 그 전인 i-2부터
-            if robot[i] == 1 and robot[i + 1] == 0 and belt[i + 1] >= 1:
-                robot[i + 1] = 1
-                robot[i] = 0
-                belt[i + 1] -= 1
-        robot[-1] = 0  # 이 부분도 로봇 out -> 0임
-
-    if robot[0] == 0 and belt[0] >= 1:
-        robot[0] = 1
-        belt[0] -= 1
-    res += 1
-    if belt.count(0) >= k:
-        break
-
-print(res)

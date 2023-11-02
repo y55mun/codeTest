5e29624cb5787699bdb2c73bdcf6d6fbf036ba9f
User <y55mun33@gmail.com>
2023-11-02 22:18:46 +0900

    완료

--- BAEKJOON/finance_study/13_20055.py
+++ BAEKJOON/finance_study/13_20055.py
@@ -12,3 +12,33 @@ https://www.acmicpc.net/problem/20055
 3. 로봇 올리기: 올리는 위치에 내구도 != 0 and 로봇이 처음에 존재하지 X
 4. 중지: count(내구도 == 0) >= k
 """
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+a = deque(map(int, input().split()))    # 내구도
+robot = deque([0]*n)    # 벨트 위에 있는 로봇
+result = 0
+
+while True:
+    a.rotate(1)
+    robot.rotate(1)
+    robot[-1] = 0
+
+    for i in range(n-2, -1, -1):
+        if a[i+1] >=1 and robot[i+1] == 0 and robot[i] == 1:
+            robot[i+1] = 1
+            robot[i] = 0
+            a[i+1] -= 1
+    robot[-1] = 0
+
+    if a[0] !=0 and robot[0] != 1:
+        robot[0] = 1
+        a[0] -= 1
+    result += 1
+
+    if a.count(0) >= k:
+        break
+print(result)

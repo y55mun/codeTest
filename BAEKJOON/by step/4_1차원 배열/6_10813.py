f8523e1ff32d50d11019538516553d7db64cf6ac
User <y55mun33@gmail.com>
2023-11-05 21:59:13 +0900

    풀이 완료

--- BAEKJOON/by step/4_1차원 배열/6_10813.py
+++ BAEKJOON/by step/4_1차원 배열/6_10813.py
@@ -3,15 +3,13 @@ https://www.acmicpc.net/problem/10813
 """
 
 N, M = map(int, input().split())
-# basket = [0] * (N+1)
-basket = [range(N+1)]
+basket = list(range(1, N+1))
 
-for _ in range(M):
+for i in range(M):
     i, j = map(int, input().split())
 
     # i번 바구니와 j번 바구니 교환
-    basket[i], basket[j] = basket[j], basket[i]
+    basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
 
-# for i in range(1, N+1):
-#     print(basket[i], end=' ')
-print(basket[1:])
+for i in range(N):
+    print(basket[i], end=' ')

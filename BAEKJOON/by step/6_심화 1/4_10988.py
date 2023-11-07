c3b6cd02cb1ce1e389cfeac6a7e2461647b76301
User <y55mun33@gmail.com>
2023-11-07 23:14:44 +0900

    풀이 완료

--- BAEKJOON/by step/6_심화 1/4_10988.py
+++ BAEKJOON/by step/6_심화 1/4_10988.py
@@ -2,24 +2,8 @@
 https://www.acmicpc.net/problem/10988
 """
 
-voca = str(input())
+voca = list(input())
 
-ans = "1"
-for i in range(len(voca)):
-    print(voca[:(len(voca)//2)])
-    # print(voca[-1::(len(voca)//2)+1])
-    print(voca[-1:(len(voca)//2)-1:-1])
-
-
-    if len(voca) // 2: # voca 길이가 홀수 이면
-        if voca[:(len(voca)//2)] == voca[:(len(voca)//2):-1]:
-            ans = "1"
-        else:
-            ans = "0"
-    else:
-        if voca[:(len(voca)//2)] == voca[-1:(len(voca)//2)-1:-1]:
-            ans = "1"
-        else:
-            ans = "0"
-
-print(ans)
+if voca == voca[::-1]:
+    print("1")
+else: print("0")

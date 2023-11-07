08d9f917279d39151e2c3bff86df9a7863d6f6b4
User <y55mun33@gmail.com>
2023-11-07 22:05:17 +0900

    안됨 - 5번째 이후 줄이 이상함

---
+++ BAEKJOON/by step/6_심화 1/3_2444.py
@@ -0,0 +1,9 @@
+"""  별 찍기 - 7
+https://www.acmicpc.net/problem/2444
+"""
+
+n = int(input())
+for i in range(1, n+1):
+    print(' '*(n-i)+'*'*(2*i-1))
+for j in range(n-1, 0, -1):
+    print(' '*(n-i)+'*'*(2*i-1))

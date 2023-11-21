7a35658d2fa84ada6d45c40e45ab13a639358393
User <y55mun33@gmail.com>
2023-11-22 00:34:16 +0900

    풀이 완료 - 주석 수정

--- BAEKJOON/by step/8_일반 수학 1/5_2292.py
+++ BAEKJOON/by step/8_일반 수학 1/5_2292.py
@@ -8,6 +8,6 @@ nums = 1    # 초기 숫자 1의 벌집 갯수
 for i in range(N):
     nums += i*6 # i번째 벌집 개수는 6의 배수로 늘어남. nums: 벌집 개수 누적
 
-    if N<nums:
-        print(i+1)
+    if N <= nums:   # 벌집 개수의 누적이 입력값보다 커지면
+        print(i+1)  # i+1 번째 겹 인것이므로 출력
         break

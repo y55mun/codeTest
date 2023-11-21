36f22e4a814ba6bfcb88c89b09a410568af91d23
User <y55mun33@gmail.com>
2023-11-22 00:11:50 +0900

    풀이 완료 - 주석 추가

--- BAEKJOON/by step/8_일반 수학 1/5_2292.py
+++ BAEKJOON/by step/8_일반 수학 1/5_2292.py
@@ -3,10 +3,10 @@ https://www.acmicpc.net/problem/2292
 """
 
 N = int(input())
-nums = 1
+nums = 1    # 초기 숫자 1의 벌집 갯수
 
 for i in range(N):
-    nums += i*6
+    nums += i*6 # i번째 벌집 개수는 6의 배수로 늘어남. nums: 벌집 개수 누적
 
     if N<nums:
         print(i+1)

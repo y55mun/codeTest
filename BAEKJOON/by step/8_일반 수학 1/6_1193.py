9d0ba03426f11c779ffa25b603bd5010bb2b739b
User <y55mun33@gmail.com>
2023-11-24 21:44:04 +0900

    풀이 완료 - 주석 추가

--- BAEKJOON/by step/8_일반 수학 1/6_1193.py
+++ BAEKJOON/by step/8_일반 수학 1/6_1193.py
@@ -6,13 +6,16 @@ num = int(input())
 line = 1
 
 while num > line:
-    num -= line
+    num -= line     # 각 line 에서 n이 몇 번째에 위치하는지 알 수 있다.
     line += 1
 
+# 짝수 일 때
 if line % 2 == 0:
     a = num
     b = line - num + 1
-else:
+
+# 홀수 일 때때
+ele:
     a = line-num+1
     b = num
 

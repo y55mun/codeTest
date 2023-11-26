e80c2c2cd8af2f65cf607dfc023db4ceb55edf5d
User <y55mun33@gmail.com>
2023-11-26 22:25:34 +0900

    풀이 완료 - 주석 추가

--- BAEKJOON/by step/9_약수_배수와 소수/4_1978.py
+++ BAEKJOON/by step/9_약수_배수와 소수/4_1978.py
@@ -14,9 +14,9 @@ a에 있는 원소가 소수인지 판별
 for num in a:
     if num > 1:
         # 소수 판별
-        for j in range(2, num+1):
+        for j in range(2, num+1):   # 1은 소수가 아니니 제외
             if num % j == 0:
                 if num == j:
                     ans += 1
-                break
+                break       # num에 도달하지 못하고 중간에 나누어떨어지는 수를 만나게 되면 해당 수는 소수가 아님
 print(ans)

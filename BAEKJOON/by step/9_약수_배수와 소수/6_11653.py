8fffab1cfca3ff9715444eddaa962f3db8745956
User <y55mun33@gmail.com>
2023-11-27 23:27:10 +0900

    풀이 완료

--- BAEKJOON/by step/9_약수_배수와 소수/6_11653.py
+++ BAEKJOON/by step/9_약수_배수와 소수/6_11653.py
@@ -3,21 +3,12 @@ https://www.acmicpc.net/problem/11653
 """
 
 n = int(input())
-cnt = 0
-ans = []
-for i in range(2, n):
-    count = 0   # 약수의 갯수
 
+if n == 1:
+    print('')
+
+for i in range(2, n+1):
     if n % i == 0:
-        count += 1
-
-        for j in range(2, i):
-            if j % i == 0:
-                count += 2
-                break
-        if count == 1:
-            n //= i
-            ans.append(i)
-
-
-print(ans)
+        while n % i == 0:
+            print(i)
+            n /= i

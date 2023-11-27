f9ee30d6b86fab2648c50f764e5c54cb3cbdb215
User <y55mun33@gmail.com>
2023-11-27 21:47:00 +0900

    이렇게 하면 답이 안나옴

---
+++ BAEKJOON/by step/9_약수_배수와 소수/5_2581.py
@@ -0,0 +1,24 @@
+""" 소수
+https://www.acmicpc.net/problem/2581
+"""
+
+m = int(input())
+n = int(input())
+ans = []
+
+count = 0
+for i in range(m, n+1):
+    # count = 0
+    if i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                count += 1
+                break
+        if count == 0:
+            ans.append(i)
+
+if len(ans) > 0:
+    print(sum(ans))
+    print(min(ans))
+else:
+    print(-1)

5739aca3eb973718cde8daf44bd614812efbe568
User <y55mun33@gmail.com>
2023-12-06 01:43:13 +0900

    init

--- BAEKJOON/finance_study/16_3986.py
+++ BAEKJOON/finance_study/16_3986.py
@@ -3,24 +3,21 @@ https://www.acmicpc.net/problem/3986
 """
 
 n = int(input())
-stack = []
+
 
 ans = 0
 
-for i in range(n):
-    s = input().rstrip()
-
+for _ in range(n):
+    s = input()
+    stack = []
 
     # 로직: 선끼리 교차하지 않으면서 각 글자를 정확히 한 개의 다른 위치에 있는 같은 글자와 비교
     # 다른 위치에 있는 글자를 찾고
     # 그 글자와 현재 위치 글자를 연결
 
-    for j in range(len(stack)):
-        if stack:
-            if s[i] == stack[-1]:
-                stack.pop()
-            else:
-                stack.append(s[i])
+    for i in range(len(s)):
+        if stack and s[i] == stack[-1]:
+            stack.pop()
         else:
             stack.append(s[i])
 

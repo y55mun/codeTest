7d4df1875e5580f05c31db19138ae99e3fdaf69d
User <y55mun33@gmail.com>
2023-12-11 23:23:22 +0900

    완료 - 주석 추가

--- BAEKJOON/finance_study/17_13549.py
+++ BAEKJOON/finance_study/17_13549.py
@@ -25,15 +25,16 @@ def bfs(x, y):
             return time[x]
 
         for nx in (x - 1, x + 1, x * 2):
-            if 0 <= nx < limit and time[nx] == 0:
-                if nx == 2 * x:
+            if 0 <= nx < limit and time[nx] == 0:   # 범위 내에 있고 탐색하지 않았을 때
+                if nx == 2 * x:     # 2*x 먼저 탐색해야 하니 큐의 맨 앞에 넣어줌
                     time[nx] = time[x]
                     q.appendleft(nx)
-                else:
+                else:       # 그렇지 않으면 이전 시간 + 1
                     time[nx] = time[x] + 1
                     q.append(nx)
 
 
+# 엣지 케이스 - if 문으로 따로 처리하는게 이득임
 if a == 0:
     print(bfs(a, b) + 1)
 else:

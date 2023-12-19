5e068c868cdf5c396aa7165187b272760b4b98d5
User <y55mun33@gmail.com>
2023-12-20 00:14:19 +0900

    안됨

--- BAEKJOON/by step/10_기하_직사각형과 삼각형/7_5073.py
+++ BAEKJOON/by step/10_기하_직사각형과 삼각형/7_5073.py
@@ -1,41 +1,18 @@
 """ 삼각형과 세 변
 https://www.acmicpc.net/problem/5073
 """
-triangle = []
-maxx = 0
 
 while True:
-    triangle = list(map(int, input().split()))
-    # print(max(triangle))
-    print(triangle)
-    maxx = max(triangle)
-    print(maxx)
-    print(triangle.remove(7))
+    a, b, c = sorted(list(map(int, input().split())))
 
-    if triangle == 0:
+    if a == b == c == 0:
         break
-    # if max(triangle) >= sum(triangle.remove(max(triangle))): # 삼각형의 조건을 만족하지 못한 경우. remove 함수 몰랐었음!!!
-    #     print("Invalid")
     # else:
-    #     if len(list(set(triangle))) == 1:
-    #         print("Equilateral")
-    #     elif len(list(set(triangle))) == 2:
-    #         print("Isosceles")
-    #     else:
-    #         print("Scalene")
-
-
-    # if a == 0 and b == 0 and c== 0:
-    #     break
-    # else:
-    #     max = a
-    #
-    #
-    #     if
-    #
-    #     if a == b == c:
-    #         print("Equilateral")
-    #     elif a != b != c:
-    #         print("Scalene")
-    #     else:
-    #         print("Isosceles")
+    if a+b <= c:
+        print("Invalid")
+    elif a == b == c:
+        print("Equilateral")
+    elif a == b or b == c or c==a:
+        print("Scalene")
+    else:
+        print("Isosceles")

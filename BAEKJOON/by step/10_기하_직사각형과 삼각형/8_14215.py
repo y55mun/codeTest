9b6d2048f72896e8b12081583ec71d16660ba063
User <y55mun33@gmail.com>
2023-12-21 00:06:51 +0900

    풀이 완료 - 주석 추가

--- BAEKJOON/by step/10_기하_직사각형과 삼각형/8_14215.py
+++ BAEKJOON/by step/10_기하_직사각형과 삼각형/8_14215.py
@@ -3,5 +3,9 @@ https://www.acmicpc.net/problem/14215
 """
 
 li = sorted(list(map(int, input().split())))    # 오름차순 정렬
+
+# 삼각형의 조건: 가장 큰 값 < 다른 두 값의 합
+# 가장 큰 값을 조절할 때는 최소한으로 조절해야 함 -> 다른 두 숫자의 합-1
+
 ans = li[0] + li[1] + min(li[2], li[0]+li[1]-1)
 print(ans)

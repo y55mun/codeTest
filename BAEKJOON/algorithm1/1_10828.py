254cffba34acf1015fc3d4d87d8be9fd71eb1fa1
User <y55mun33@gmail.com>
2024-01-02 22:21:57 +0900

    틀림 - 시간 초과

--- BAEKJOON/algorithm1/1_10828.py
+++ BAEKJOON/algorithm1/1_10828.py
@@ -1,3 +1,28 @@
 """ 스택
 https://www.acmicpc.net/problem/10828
 """
+n = int(input())
+stack = []
+
+for _ in range(n):
+    a = input().split()     # 명령어
+
+    if a[0] == 'push':
+        stack.append(a[1])
+    elif a[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif a[0] == 'size':
+        print(len(stack))
+    elif a[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif a[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])

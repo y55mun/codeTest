d2caebc5dd129bd2455e4c5e8c2cb0006471d3b6
User <y55mun33@gmail.com>
2024-01-04 00:16:25 +0900

    주석 추가

--- BAEKJOON/algorithm1/3_9012.py
+++ BAEKJOON/algorithm1/3_9012.py
@@ -3,16 +3,23 @@ https://www.acmicpc.net/problem/9012
 """
 
 t = int(input())
-stack = []
+
 
 for _ in range(t):
-    a = input().split()
+    stack = []
+    a = input()
 
-    if a == '(':
-        stack.append(a)
-    elif a == ')':
-        if stack:
-            stack.pop()
-        else:
+    for j in a:
+        if j == '(':
+            stack.append(a)
+        elif j == ')':
+            if stack:
+                stack.pop()
+            else:   # 스택에 괄호가 없을 경우
+                print("NO")
+                break
+    else:   # break 문으로 끊기지 않고 수행됐을 경우 수행
+        if not stack:   # 스택이 비어 있으면 괄호가 다 맞음
+            print("YES")
+        else:   # break 안걸었어도 스택에 괄호가 들어가 있다면 NO
             print("NO")
-            break

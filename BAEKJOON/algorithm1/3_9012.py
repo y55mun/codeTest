5ea9cdd2297b9029c24a7076ee2c8a276af4ae44
User <y55mun33@gmail.com>
2024-01-04 00:00:39 +0900

    틀림

--- BAEKJOON/algorithm1/3_9012.py
+++ BAEKJOON/algorithm1/3_9012.py
@@ -1,3 +1,18 @@
 """ 괄호
 https://www.acmicpc.net/problem/9012
 """
+
+t = int(input())
+stack = []
+
+for _ in range(t):
+    a = input().split()
+
+    if a == '(':
+        stack.append(a)
+    elif a == ')':
+        if stack:
+            stack.pop()
+        else:
+            print("NO")
+            break

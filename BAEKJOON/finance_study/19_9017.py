0b3ed7b7865afe0a9830c4ffc221c2e1452c633a
User <y55mun33@gmail.com>
2024-01-07 23:03:00 +0900

    풀이 완료

--- BAEKJOON/finance_study/19_9017.py
+++ BAEKJOON/finance_study/19_9017.py
@@ -9,11 +9,13 @@ for _ in range(t):
     temp = list(map(int, input().split()))
     count = {}  # 팀 별 주자 수
 
+    # 팀 별로 주자 수 세기
     for i in range(n):
         if temp[i] in count:
             count[temp[i]] += 1
         else:
             count[temp[i]] = 1
+
     # 제외되는 팀 구하기
     dele = {}
     for k,v in count.items():
@@ -26,14 +28,15 @@ for _ in range(t):
     for i in range(n):
         if temp[i] not in dele:
             if temp[i] in team:
-                if team[temp[i]][0] < 4:
+                if team[temp[i]][0] < 4:    # 점수는 상위 4명의 점수 까지만 합산
                     team[temp[i]][0] += 1
                     team[temp[i]][1] += idx
                 elif team[temp[i]][0] == 4:
-                    team[temp[1]][0] += 1
+                    team[temp[i]][0] += 1
                     team[temp[i]][2] = idx
             else:
-                team[temp[i]] = [1, idx, 0]
+                team[temp[i]] = [1, idx, 0] # [팀당 주자 수, 상위 4명 합, #5번째 주자 점수]
             idx += 1
 
     team = sorted(team.items(), key = lambda x:(x[1][1], x[1][2]))
+    print(team[0][0])

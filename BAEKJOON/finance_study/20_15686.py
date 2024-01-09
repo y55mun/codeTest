45338ebc51642dde98fdc8e6327dc9fabcf7bf80
User <y55mun33@gmail.com>
2024-01-10 00:05:47 +0900

    .

--- BAEKJOON/finance_study/20_15686.py
+++ BAEKJOON/finance_study/20_15686.py
@@ -5,24 +5,3 @@ https://www.acmicpc.net/problem/15686
 m개의 치킨집을 선택하는 모든 조합에 대해 도시의 치킨 거리를 구한다!!
 치킨 거리: abs(r1-r2) + abs(c1-c2)
 """
-
-import sys
-from itertools import combinations
-
-n, m = map(int, input().split())
-cities = [list(map(int, input().split())) for _ in range(n)]
-
-house = [(i,j) for i in range(n) for j in range(n) if cities[i][j] == 1]
-chicken = [(i,j) for i in range(n) for j in range(n) if cities[i][j] == 2]
-result = 999999
-
-for chi in combinations(chicken, m):    # m개의 치킨집 선택
-    temp = 0    # 도시의 치킨 거리
-
-    for h in house:
-        chi_len = 999   # 각 집마다 치킨 거리
-        for j in range(m):
-            chi_len = min(chi_len, abs(h[0]-chi[j][0])+abs(h[1] - chi[j][1]))
-        temp += chi_len
-    result = min(result, temp)
-print(result)

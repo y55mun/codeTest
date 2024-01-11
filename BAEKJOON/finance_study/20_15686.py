f5fe53a3b0d1210b41deb098c78873b5c517ef8b
User <y55mun33@gmail.com>
2024-01-12 01:10:37 +0900

    풀이 완료 - 주석 추가

--- BAEKJOON/finance_study/20_15686.py
+++ BAEKJOON/finance_study/20_15686.py
@@ -16,11 +16,11 @@ house = [(i,j) for i in range(n) for j in range(n) if cities[i][j] == 1]
 chicken = [(i,j) for i in range(n) for j in range(n) if cities[i][j] == 2]
 result = 999999
 
-for chi in combinations(chicken, m):
-    temp = 0
+for chi in combinations(chicken, m):    # m개의 치킨집 선택
+    temp = 0    # 도시의 치킨 거리
 
     for h in house:
-        chi_len = 999
+        chi_len = 999   # 각 집마다 치킨 거리
         for j in range(m):
             chi_len = min(chi_len, abs(h[0]-chi[j][0])+abs(h[1] - chi[j][1]))
         temp += chi_len

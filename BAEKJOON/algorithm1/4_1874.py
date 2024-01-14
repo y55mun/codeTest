3dec6b78efe6f178353ef90d34c1cc2554a0d243
User <y55mun33@gmail.com>
2024-01-14 23:39:58 +0900

    풀이 완료

--- BAEKJOON/algorithm1/4_1874.py
+++ BAEKJOON/algorithm1/4_1874.py
@@ -1,3 +1,26 @@
 """ 스택 수열
 https://www.acmicpc.net/problem/1874
 """
+
+n = int(input())
+stack, ans, find = [], [], True
+
+now = 1
+for _ in range(n):
+    num = int(input())
+
+    while now <= num:
+        stack.append(now)
+        ans.append('+')
+        now += 1
+    if stack[-1] == num:
+        stack.pop()
+        ans.append('-')
+    else:
+        find = False
+
+if not find:
+    print('NO')
+else:
+    for i in ans:
+        print(i)

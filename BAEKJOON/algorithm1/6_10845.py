e5769d15d0adcfb05c5ded573202386352e8136a
User <y55mun33@gmail.com>
2024-01-16 23:17:07 +0900

    틀림

--- BAEKJOON/algorithm1/6_10845.py
+++ BAEKJOON/algorithm1/6_10845.py
@@ -1,3 +1,38 @@
 """ 큐
 https://www.acmicpc.net/problem/10845
 """
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+dq = deque()
+
+n = int(input())
+for _ in range(n):
+    command = input().split()
+
+    if command[0] == 'push':
+        dq.append(command[1])
+    elif command[0] == 'pop':
+        if len(dq) != 0:
+            dq.popleft()
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(dq))
+    elif command[0] == 'empty':
+        if len(dq) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(dq) != 0:
+            print(dq[0])
+        else:
+            print(-1)
+    elif command[0] == 'back':
+        if len(dq) != 0:
+            print(dq[-1])
+        else:
+            print(-1)

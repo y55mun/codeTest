7c5b492c3a2ac641898b12a42aa01a42822f8931
User <y55mun33@gmail.com>
2024-01-24 00:42:37 +0900

    풀이 완료 - 주석 추가

--- BAEKJOON/etc/7_14501.py
+++ BAEKJOON/etc/7_14501.py
@@ -8,17 +8,17 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-d = [0]*(n+2)
-t = [0]*(n+1)
-p = [0]*(n+1)
+d = [0]*(n+2)   # 오늘 ~ 퇴사일까지 벌 수 있는 최대 수입
+t = [0]*(n+1)   # 상담에 필요한 일수
+p = [0]*(n+1)   # 상담 완료 시 수입
 
 for i in range(1, n+1):
     t[i], p[i] = map(int, input().split())
 
 for i in range(n, 0, -1):
-    if i+t[i] > n+1:
+    if i+t[i] > n+1:    # i번째 상담을 퇴사일까지 끝낼 수 없을 때
         d[i] = d[i+1]
-    else:
+    else:   # i번째 상담을 퇴사일까지 끝낼 수 있을 때
         d[i] = max(d[i+1], p[i]+d[i+t[i]])
 
 print(d[1])

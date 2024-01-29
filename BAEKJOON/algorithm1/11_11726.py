ca9db174c68c6c044cc29cd1ca8f5a1f4d92df6e
User <y55mun33@gmail.com>
2024-01-29 21:52:39 +0900

    풀이 완료

---
+++ BAEKJOON/algorithm1/11_11726.py
@@ -0,0 +1,14 @@
+""" 2×n 타일링
+https://www.acmicpc.net/problem/11726
+"""
+
+n = int(input())
+
+d = [0] * (1001)
+d[1] = 1
+d[2] = 2
+
+for i in range(3, n+1):
+    d[i] = (d[i-1] + d[i-2]) % 10007
+
+print(d[n])

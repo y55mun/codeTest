83edc4f8614b74b59a2a478e4ef0a43bc2e439ef
User <y55mun33@gmail.com>
2024-01-29 20:27:34 +0900

    풀이 완료

---
+++ BAEKJOON/algorithm1/10_2193.py
@@ -0,0 +1,17 @@
+""" 이친수
+https://www.acmicpc.net/problem/2193
+"""
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = [[0 for j in range(2)] for i in range(n+1)]
+d[1][1] = 1
+d[1][0] = 0
+
+for i in range(2, n+1):
+    d[i][0] = d[i-1][1] + d[i-1][0]
+    d[i][1] = d[i-1][0]
+
+print(d[n][0] + d[n][1])

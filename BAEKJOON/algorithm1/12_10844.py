4c1eb24e47e7db3b8d75dced7fb1adcd396909df
User <y55mun33@gmail.com>
2024-02-01 23:48:17 +0900

    .

--- BAEKJOON/algorithm1/12_10844.py
+++ BAEKJOON/algorithm1/12_10844.py
@@ -1,3 +1,10 @@
 """ 쉬운 계단 수
 https://www.acmicpc.net/problem/10844
 """
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = [[0 for j in range(11)] for i in range(n+1)]
+print(d)
---
+++ BAEKJOON/finance_study/24_11048.py
@@ -0,0 +1,6 @@
+""" 이동하기
+https://www.acmicpc.net/problem/11048
+"""
+
+n, m = map(int, input().split())
+miro = [list(map(int, input().split())) for i in range(n)]

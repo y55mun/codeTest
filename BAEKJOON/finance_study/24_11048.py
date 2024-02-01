b50642f338e8caae46738c7d47dc6a7718cf70a6
User <y55mun33@gmail.com>
2024-02-01 23:57:59 +0900

    풀이 완료

--- BAEKJOON/finance_study/24_11048.py
+++ BAEKJOON/finance_study/24_11048.py
@@ -1,6 +1,14 @@
 """ 이동하기
 https://www.acmicpc.net/problem/11048
 """
+import sys
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-miro = [list(map(int, input().split())) for i in range(n)]
+candy = [list(map(int, input().split())) for i in range(n)]
+dp = [[0] * (m+1)] * (n+1)
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + candy[i-1][j-1]
+print(dp[n][m])

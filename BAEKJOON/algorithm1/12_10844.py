0f4306c78837bb51db18a7b773d25e084e736c4a
User <y55mun33@gmail.com>
2024-02-07 23:30:11 +0900

    풀이

--- BAEKJOON/algorithm1/12_10844.py
+++ BAEKJOON/algorithm1/12_10844.py
@@ -7,4 +7,22 @@ input = sys.stdin.readline
 
 n = int(input())
 d = [[0 for j in range(11)] for i in range(n+1)]
-print(d)
+mod = 1000000000
+
+for i in range(1,10):
+    d[1][i] = 1
+
+for i in range(2, n+1):
+    d[i][0] = d[i-1][1]
+    d[i][9] = d[i-1][8]
+
+    for j in range(1,9):
+        d[i][j] = (d[i-1][j-1] + d[i-1][j+1]) % mod
+
+sum = 0
+
+for i in range(10):
+    sum = (sum+d[n][i]) % mod
+
+print(sum)
+

f9e257a4852b24b981cafe7edc8a73b8ccb4ff92
User <y55mun33@gmail.com>
2024-02-14 23:54:14 +0900

    완료

--- BAEKJOON/algorithm1/review.py
+++ BAEKJOON/algorithm1/review.py
@@ -12,3 +12,10 @@ p = [0] * (n+1)
 
 for i in range(1, n+1):
     t[i], p[i] = map(int, input().split())
+
+for i in range(n, 0, -1):
+    if i+t[i] > n+1:
+        d[i] = d[i+1]
+    else:
+        d[i] = max(d[i+1], p[i]+d[i+t[i]])
+print(d[1])

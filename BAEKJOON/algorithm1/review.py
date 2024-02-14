f322fb7f47f8a31bba8cbc7ead3aa5c74c742cd7
User <y55mun33@gmail.com>
2024-02-14 23:52:50 +0900

    ing - input 까지만 가능

--- BAEKJOON/algorithm1/review.py
+++ BAEKJOON/algorithm1/review.py
@@ -1,3 +1,14 @@
 """ 퇴사
 https://www.acmicpc.net/problem/14501
 """
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = [0] * (n+2)
+t = [0] * (n+1)
+p = [0] * (n+1)
+
+for i in range(1, n+1):
+    t[i], p[i] = map(int, input().split())

d673ed6c2b4326e21ba58a0b2d05e8e94f7a86bb
User <y55mun33@gmail.com>
2024-02-16 00:42:33 +0900

    풀이 완료 - 주석 추가

--- BAEKJOON/finance_study/25_11060.py
+++ BAEKJOON/finance_study/25_11060.py
@@ -11,8 +11,10 @@ dp = [n+1] * n
 dp[0] = 0
 
 for i in range(n):
+    # 점프로 갈 수 있는 칸 확인
     for j in range(1, miro[i]+1):
         if i+j < n:
+            # 점프한 칸에 점프 횟수에 값을 최솟값으로 초기화
             dp[i+j] = min(dp[i+j], dp[i]+1)
 
 print(dp[n-1] if dp[n-1] != n+1 else -1)

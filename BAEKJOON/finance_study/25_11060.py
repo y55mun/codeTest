d53f8d00e8fda0fe034c73894a9f871db1e05e74
User <y55mun33@gmail.com>
2024-02-16 00:17:49 +0900

    풀이 완료

--- BAEKJOON/finance_study/25_11060.py
+++ BAEKJOON/finance_study/25_11060.py
@@ -1,3 +1,18 @@
 """ 점프 점프
 https://www.acmicpc.net/problem/11060
 """
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+miro = list(map(int, input().split()))
+dp = [n+1] * n
+dp[0] = 0
+
+for i in range(n):
+    for j in range(1, miro[i]+1):
+        if i+j < n:
+            dp[i+j] = min(dp[i+j], dp[i]+1)
+
+print(dp[n-1] if dp[n-1] != n+1 else -1)

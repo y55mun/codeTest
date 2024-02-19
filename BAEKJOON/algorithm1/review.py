f31de26d2321d504b6ba0a782ea42fddfbf811d5
User <y55mun33@gmail.com>
2024-02-19 23:10:50 +0900

    .

--- BAEKJOON/algorithm1/review.py
+++ BAEKJOON/algorithm1/review.py
@@ -1,21 +1,10 @@
-""" 퇴사
-https://www.acmicpc.net/problem/14501
+""" 이친수
+https://www.acmicpc.net/problem/2193
 """
 
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-d = [0] * (n+2)
-t = [0] * (n+1)
-p = [0] * (n+1)
-
-for i in range(1, n+1):
-    t[i], p[i] = map(int, input().split())
-
-for i in range(n, 0, -1):
-    if i+t[i] > n+1:
-        d[i] = d[i+1]
-    else:
-        d[i] = max(d[i+1], p[i]+d[i+t[i]])
-print(d[1])
+d = [[0 for j in range(2)] for i in range(n+1)]
+d[1][1] = 1

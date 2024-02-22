fc3923eca90821fe14fc2de476f1beac19481974
User <y55mun33@gmail.com>
2024-02-23 01:21:21 +0900

    1차 풀이

--- codingTest_AllInOne/1.py
+++ codingTest_AllInOne/1.py
@@ -1,3 +1,19 @@
 """
 https://leetcode.com/problems/valid-parentheses/description/
 """
+
+s = input()
+
+
+stack = []
+for p in s:
+    if p == '{':
+        stack.append("}")
+    elif p == '[':
+        stack.append("]")
+    elif p == '(':
+        stack.append(")")
+    elif stack and stack[-1] == p:
+        stack.pop()
+    else:
+        print("False")

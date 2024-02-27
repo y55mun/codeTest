9b4e2d7628156aecac35df7daba9d1d8483e82ca
User <y55mun33@gmail.com>
2024-02-27 23:52:18 +0900

    풀이 완료

--- codingTest_AllInOne/2.py
+++ codingTest_AllInOne/2.py
@@ -2,3 +2,21 @@
 https://leetcode.com/problems/longest-consecutive-sequence/description/
 """
 
+longest = 0
+num_dict = {}
+
+nums = map(int, input().split())
+
+for num in nums:
+    num_dict[num] = True
+
+for num in num_dict:
+    if num-1 not in num_dict:
+        cnt = 1
+        target = num+1
+
+        while target in num_dict:
+            target += 1
+            cnt += 1
+        longest = max(longest, cnt)
+print(longest)

7110868292b8a2b99d1bef7464163c43b7b0fdf5
User <y55mun33@gmail.com>
2024-02-28 00:41:05 +0900

    풀이 완료 - 주석 추가

--- BAEKJOON/by step/2_4375.py
+++ BAEKJOON/by step/2_4375.py
@@ -9,13 +9,13 @@ while True:
         break
 
     num = 1
-    num_count = 1
+    num_count = 1   # 자리수
 
     while True:
-        if num % n != 0:
-            num = num*10 + 1
-            num_count += 1
-        else:
+        if num % n != 0:    # n의 배수일 경우
+            num = num*10 + 1    # 1로만 이루어진 다음 수로 갱신
+            num_count += 1  # 자리수 카운트
+        else:   # n의 배수가 아닐 경우 종료
             break
     print(num_count)
 

6c18c6ea248b74f08629caec269939b1bda3edc2
User <y55mun33@gmail.com>
2024-02-28 00:40:20 +0900

    풀이 완료

--- BAEKJOON/by step/2_4375.py
+++ BAEKJOON/by step/2_4375.py
@@ -2,5 +2,20 @@
 https://www.acmicpc.net/problem/4375
 """
 
-n = list(input())
-print(n)
+while True:
+    try:
+        n = int(input())
+    except:
+        break
+
+    num = 1
+    num_count = 1
+
+    while True:
+        if num % n != 0:
+            num = num*10 + 1
+            num_count += 1
+        else:
+            break
+    print(num_count)
+

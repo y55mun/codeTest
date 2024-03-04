d22a8521f887e0b5225e551d96129f4bed016c22
User <y55mun33@gmail.com>
2024-03-05 00:34:32 +0900

    .

--- BAEKJOON/finance_study/3.py
+++ BAEKJOON/finance_study/3.py
@@ -1,30 +1,18 @@
-"""
-https://school.programmers.co.kr/learn/courses/15008/lessons/121685
-"""
+from collections import deque
 
-queries = list(map(int, input().split()))
-ans = []
-beans ={1: "RR", 2:"Rr", 3: "Rr", 4:"rr"}
+def bfs(root):
+    visited = []
+    if root is None:
+        return 0
+    q = deque()
+    q.append(root)
 
-def find(n,p):
-    beans
+    while q:
+        cur_node = q.popleft()
+        visited.append(cur_node.value)
 
-    if n <= 2:
-        ans = beans[p]
-        return ans
-    N = 4**(n-2)
-    if p <= N:
-        ans = "RR"
-    elif p <= 2*N:
-        ans = find(n-1, p-N)
-    elif p <= 3*N:
-        ans = find(n-1, p-2*N)
-    else:
-        ans = "rr"
-    return ans
-
-for n, p in queries:
-    ans = find(n,p) if n>1 else "Rr"
-    ans.append(ans)
-
-print(ans)
+        if cur_node.left:
+            q.append(cur_node.left)
+        if cur_node.right:
+            q.append(cur_node.right)
+    return visited

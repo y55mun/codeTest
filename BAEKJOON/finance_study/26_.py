afdd14aa32ee84a1704612f6dd17f13db5aeb958
User <y55mun33@gmail.com>
2024-03-12 22:19:06 +0900

    완료 - 주석 추가

--- BAEKJOON/finance_study/26_.py
+++ BAEKJOON/finance_study/26_.py
@@ -3,10 +3,10 @@ https://school.programmers.co.kr/learn/courses/15009/lessons/121687
 """
 
 def solution(command):
-    dxy = [[0,1], [1,0], [0,-1], [-1, 0]]
-    x = 0
-    y = 0
-    dir = 0
+    dxy = [[0,1], [1,0], [0,-1], [-1, 0]]   # 12시, 3시, 6시, 9시 방향으로 회전
+    x = 0   # x좌표
+    y = 0   # y좌표
+    dir = 0 # 방향 (dxy 배열의 dir 번째 배열을 선택해 좌표 이동)
 
     for c in command:
         if c == 'R':    # 오른쪽으로 회전

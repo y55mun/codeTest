35d6cc83e58fd4e60d2564954aaa1aba5898b847
User <y55mun33@gmail.com>
2024-03-12 22:01:59 +0900

    완료

--- BAEKJOON/finance_study/26_.py
+++ BAEKJOON/finance_study/26_.py
@@ -2,22 +2,21 @@
 https://school.programmers.co.kr/learn/courses/15009/lessons/121687
 """
 
-command = "GRGRGRB"
+def solution(command):
+    dxy = [[0,1], [1,0], [0,-1], [-1, 0]]
+    x = 0
+    y = 0
+    dir = 0
 
-dxy = [[0,1], [1,0], [0,-1], [0,1]]
-x = 0
-y = 0
-dir = 0
-
-for c in command:
-    if c == 'R':    # 오른쪽으로 회전
-        dir = (dir+1)%4
-    elif c == 'L':
-        dir = (dir-1)%4
-    elif c == 'G':
-        x += dxy[dir][0]
-        y += dxy[dir][1]
-    else:
-        x -= dxy[dir][0]
-        y -= dxy[dir][1]
-print([x,y])
+    for c in command:
+        if c == 'R':    # 오른쪽으로 회전
+            dir = (dir+1)%4
+        elif c == 'L':
+            dir = (dir-1)%4
+        elif c == 'G':
+            x += dxy[dir][0]
+            y += dxy[dir][1]
+        else:
+            x -= dxy[dir][0]
+            y -= dxy[dir][1]
+    return [x,y]

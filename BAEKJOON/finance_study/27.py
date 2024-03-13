1268755c075f63da1981fc809e7910a21eefef89
User <y55mun33@gmail.com>
2024-03-13 22:27:10 +0900

    풀이 완료

--- BAEKJOON/finance_study/27.py
+++ BAEKJOON/finance_study/27.py
@@ -8,42 +8,20 @@ order = [1, 2, 0, 1]
 k = 10
 ###########################
 
-from collections import deque
 
-ans = 0
-n = len(order)
-q = deque()
-i = 0
-time = 0
+order_list = []
+current = []
 
-while q or i <n:
-    if not q:   # 큐가 비어 있다면
-        time = (i*k) + menu[order[i]]   # 시간을 도착시간 + 주문시간 으로 변경
-        i += 1
-    else:   # 큐가 비어있지 않다면
-        x = q.popleft()     # 큐에서 하나를 꺼내서
-        time += menu[x]     # 시간에 주문 시간을 더함
+for i in range(len(order)):
+    order_list.append(menu[order[i]])
 
-    # while i < n and i <= ((time-1) // k):   # 입장한 사람이 있을 경우
-    while i < n and i <= len(order) :
-        q.append(order[i])  # 큐에 추가
-        i += 1
-    ans = max(ans, len(q))
-# return (ans + 1)
-print(ans)
+    current.append(len(order_list))
 
+    order_list[0] = order_list[0] - k
 
-## Tree 디버깅 시 사용######
-root = [3, 9, 20, None, None, 15, 7]
+    while order_list and order_list[0] <= 0:
+        if len(order_list) >= 2:
+            order_list[1] += order_list[0]
+        order_list.pop(0)
 
-class TreeNode:
-    def __init__(self, l=None, r=None, v=0):
-        self.left = l
-        self.right = r
-        self.value = v
-
-root = TreeNode(v=3)
-root.left = TreeNode(v=9)
-root.right = TreeNode(v=20)
-root.right.left = TreeNode(v=15)
-root.right.right = TreeNode(v=7)
+print(max(current))

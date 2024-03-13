2075d21dfadd1630d9156271d9019b20b79b01e4
User <y55mun33@gmail.com>
2024-03-13 22:43:25 +0900

    풀이 완료 - 주석 추가

--- BAEKJOON/finance_study/27.py
+++ BAEKJOON/finance_study/27.py
@@ -13,15 +13,17 @@ order_list = []
 current = []
 
 for i in range(len(order)):
-    order_list.append(menu[order[i]])
+    order_list.append(menu[order[i]])   # 주문 리스트 추가
 
-    current.append(len(order_list))
+    current.append(len(order_list)) # 현재의 주문수를 리스트에 추가
 
-    order_list[0] = order_list[0] - k
-
+    order_list[0] = order_list[0] - k   # 제일 앞 손님의 음료 제조 시간 차감
+    
+    # 차감 결과가 음수 라면 바로 다음 주문의 제조 시간 차감
+    # 음수가 된 주문 리스트에서 제거하여 다음 손님이 제일 앞 손님이 됨
     while order_list and order_list[0] <= 0:
         if len(order_list) >= 2:
             order_list[1] += order_list[0]
         order_list.pop(0)
 
-print(max(current))
+print(max(current)) # 주문 수 목록에서 제일 큰 수 리턴

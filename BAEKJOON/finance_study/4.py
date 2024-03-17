ad162e7bd063922e4e8f407b17783344ae022d9c
User <y55mun33@gmail.com>
2024-03-18 01:27:23 +0900

    풀이 완료

--- BAEKJOON/finance_study/4.py
+++ BAEKJOON/finance_study/4.py
@@ -2,15 +2,23 @@
 https://school.programmers.co.kr/learn/courses/15009/lessons/121688
 
 아이디어
-- 원소 중에 가장 작은 원소 2개 찾기
+- 원소 중에 가장 작은 원소 2개 찾기 -> sort 를 사용하면 매 순간마다 정렬을 해야해서 시간복잡도가 커짐.
 - 두 원소의 합을 구해서 해당 원소들을 업데이트
 """
+
+import heapq
 
 ability = [1, 2, 3, 4]
 number = 3
 
-for _ in range(number):
-    ability.sort()  # 가장 작은 순서대로 나열
-    ability[0:2] = sum(ability[0:2]), sum(ability[0:2])
+q = []
+for a in ability:
+    heapq.heappush(q, a)
 
-print(sum(ability))
+for _ in range(number):
+    x, y = heapq.heappop(q), heapq.heappop(q)
+    new = x + y
+    heapq.heappush(q, new)
+    heapq.heappush(q, new)
+
+return sum(q)

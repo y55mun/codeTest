1ab2dcb65fbe5b5e25b4e17eff722af8eaa9b34f
User <y55mun33@gmail.com>
2024-03-27 00:35:47 +0900

    풀이 완료

--- BAEKJOON/finance_study/26_2133.py
+++ BAEKJOON/finance_study/26_2133.py
@@ -1,3 +1,14 @@
 """ 타일 채우기
 https://www.acmicpc.net/problem/2133
 """
+
+n = int(input())
+dp = [0 for _ in range(31)]
+dp[2] = 3
+
+for i in range(4, n+1):
+    if i%2 == 0:
+        dp[i] = dp[i-2]*3 + sum(dp[:i-2])*2 + 2
+    else:
+        dp[i] = 0
+print(dp[n])

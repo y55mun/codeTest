a354cd86d333aebfadc885cc429dc93e99fe49b4
User <y55mun33@gmail.com>
2024-03-31 15:20:45 +0900

    완료

--- BAEKJOON/finance_study/28_2206.py
+++ BAEKJOON/finance_study/28_2206.py
@@ -4,33 +4,43 @@ https://www.acmicpc.net/problem/2206
 
 from collections import deque
 
-shortest_path_len = -1
-# row_len, col_len = map(int, input().split())
-# grid = [list(map(int, input())) for _ in range(row_len)]
+def bfs():
+    q = deque()
+    q.append([0,0,0])
 
-#####테스트용
-row_len, col_len = 6, 4
-grid = [[0,1,0,0], [1,1,1,0], [1,0,0,0], [0,0,0,0], [0,1,1,1], [0,0,0,0]]
+    while q:
+        cur_r, cur_c, breakable = q.popleft()
 
-#####################
-visited = [[False] * col_len for _ in range(row_len)]
+        if cur_r == row_len - 1 and cur_c == col_len - 1:
+            return visited[cur_r][cur_c][breakable]
+
+        for dr, dc in directions:
+            next_r = cur_r + dr
+            next_c = cur_c + dc
+
+            if next_r <= -1 or next_r >= row_len or next_c <= -1 or next_c >= col_len:
+                continue
+
+            # 다음 이동할 곳이 벽이고 벽 파괴 기회를 사용하지 않은 경우 -> 벽 부수기
+            if grid[next_r][next_c] == 1 and breakable == 0:
+                visited[next_r][next_c][1] = visited[cur_r][cur_c][0] + 1
+                q.append([next_r, next_c, 1])
+
+            # 다음 이동할 곳이 벽이 아니고 한번도 방문하지 않음
+            elif grid[next_r][next_c] == 0 and visited[next_r][next_c][breakable] == 0:
+                visited[next_r][next_c][breakable] = visited[cur_r][cur_c][breakable] + 1
+                q.append([next_r, next_c, breakable])
+
+    return -1
+
+
+row_len, col_len = map(int, input().split())
+grid = [list(map(int, input())) for _ in range(row_len)]
+
+# visited = [[[0, 0] * col_len] for _ in range(row_len)]
+visited = [[[0] * 2 for _ in range(col_len)] for _ in range(row_len)]
+visited[0][0][0] = 1
+
 directions = [(1,0), (-1,0), (0,1), (0,-1)]
 
-q = deque()
-q.append((0,0,1))
-visited[0][0] = True
-
-while q:
-    cur_r, cur_c, cur_len = q.popleft()
-    if cur_r == row_len-1 and cur_c == col_len-1:
-        shortest_path_len = cur_len
-        break
-    for dr, dc in directions:
-        next_r = cur_r + dr
-        next_c = cur_c + dc
-
-        if 0 <= next_r < row_len and 0 <= next_c < col_len:
-            if grid[next_r][next_c] == 0 and not visited[next_r][next_c]:
-                q.append((next_r, next_c, cur_len+1))
-                visited[next_r][next_c] = True
-print(shortest_path_len)
+print(bfs())

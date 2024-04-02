0f9cdb43ad9a8d906c36d41dd7942d277da930b8
User <y55mun33@gmail.com>
2024-04-02 23:15:05 +0900

    .

--- BAEKJOON/finance_study/30.py
+++ BAEKJOON/finance_study/30.py
@@ -33,9 +33,3 @@ def solution(bandage, health, attacks):
 
         cur_health = min(cur_health, max_health)
     return cur_health
-
-bandage = [5, 1, 5]
-health = 30
-attacks = [[2, 10], [9, 15], [10, 5], [11, 5]]
-
-print(solution(bandage, health, attacks))
